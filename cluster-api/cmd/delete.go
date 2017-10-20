5422ae87c5e57a006ab5dbbc7d575d2eb26d2531
Ajit Kumar <kumaraji@google.com>
2017-10-20 15:00:28 -0700

    Add delete command

--- cluster-api/cmd/delete.go
+++ cluster-api/cmd/delete.go
@@ -1,1 +1,71 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/kris-nova/kubicorn/cutil"
+	"github.com/kris-nova/kubicorn/cutil/initapi"
+	"github.com/kris-nova/kubicorn/cutil/logger"
+	"github.com/kris-nova/kubicorn/cutil/task"
+	"github.com/spf13/cobra"
+	"k8s.io/kube-deploy/cluster-api/api"
+	"os"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete [YAML_FILE]",
+	Short: "Simple kubernetes cluster manager",
+	Long:  `Delete a kubernetes cluster with one command`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			logger.Critical("Please provide yaml file for cluster definition.")
+			os.Exit(1)
+		} else if len(args) > 1 {
+			logger.Critical("Too many arguments.")
+			os.Exit(1)
+		}
+		yamlFile := args[0]
+		cluster, err := readAndValidateYaml(yamlFile)
+		if err != nil {
+			logger.Critical(err.Error())
+			os.Exit(1)
+		}
+		logger.Info("Parsing done [%s]", cluster)
+
+		if err = deleteCluster(cluster); err != nil {
+			logger.Critical(err.Error())
+			os.Exit(1)
+		}
+
+	},
+}
+
+// deleteCluster uses kubicorn API to delete cluster.
+func deleteCluster(cluster *api.Cluster) error {
+	newCluster := convertToKubecornCluster(cluster)
+
+	newCluster, err := initapi.InitCluster(newCluster)
+	if err != nil {
+		return err
+	}
+	runtimeParams := &cutil.RuntimeParameters{}
+	reconciler, err := cutil.GetReconciler(newCluster, runtimeParams)
+	if err != nil {
+		return fmt.Errorf("Unable to get reconciler: %v", err)
+	}
+
+	var deleteClusterTask = func() error {
+		_, err = reconciler.Destroy()
+		return err
+	}
+
+	err = task.RunAnnotated(deleteClusterTask, fmt.Sprintf("\nDestroying resources for cluster [%s]:\n", newCluster.Name), "")
+	if err != nil {
+		return fmt.Errorf("Unable to destroy resources for cluster [%s]: %v", newCluster.Name, err)
+	}
+
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(deleteCmd)
+}
--- cluster-api/cmd/root.go
+++ cluster-api/cmd/root.go
@@ -1,17 +1,22 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
 	"fmt"
+	"github.com/ghodss/yaml"
+	"github.com/kris-nova/kubicorn/apis/cluster"
+	"github.com/kris-nova/kubicorn/cutil/logger"
+	"github.com/kris-nova/kubicorn/profiles"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"k8s.io/kube-deploy/cluster-api/api"
 	"os"
 	"os/exec"
-	"github.com/kris-nova/kubicorn/cutil/logger"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "cluster-api",
 	Short: "Simple kubernetes cluster management",
-	Long: `Simple kubernetes cluster management`,
+	Long:  `Simple kubernetes cluster management`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 		cmd.Help()
@@ -21,7 +26,6 @@ var RootCmd = &cobra.Command{
 func init() {
 	logger.Level = 4
 }
-
 
 func execCommand(name string, args []string) string {
 	cmdOut, _ := exec.Command(name, args...).Output()
@@ -33,4 +37,43 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+}
+
+func convertToKubecornCluster(cluster *api.Cluster) *cluster.Cluster {
+	newCluster := profileMapIndexed[cluster.Spec.Cloud](cluster.Name)
+	newCluster.Name = cluster.Name
+	newCluster.CloudId = cluster.Spec.Project
+	newCluster.SSH.User = cluster.Spec.SSH.User
+	newCluster.SSH.PublicKeyPath = cluster.Spec.SSH.PublicKeyPath
+	return newCluster
+}
+
+func readAndValidateYaml(file string) (*api.Cluster, error) {
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	clusterSpec := &api.Cluster{}
+	err = yaml.Unmarshal(bytes, clusterSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := profileMapIndexed[clusterSpec.Spec.Cloud]; !ok {
+		return nil, fmt.Errorf("invalid cloud option [%s]", clusterSpec.Spec.Cloud)
+	}
+	if clusterSpec.Spec.Cloud == cluster.CloudGoogle && clusterSpec.Spec.Project == "" {
+		return nil, fmt.Errorf("CloudID is required for google cloud. Please set it to your project ID")
+	}
+	return clusterSpec, nil
+}
+
+type profileFunc func(name string) *cluster.Cluster
+
+var profileMapIndexed = map[string]profileFunc{
+	"azure":        profiles.NewUbuntuAzureCluster,
+	"azure-ubuntu": profiles.NewUbuntuAzureCluster,
+	"google":       profiles.NewUbuntuGoogleComputeCluster,
+	"gcp":          profiles.NewUbuntuGoogleComputeCluster,
 }

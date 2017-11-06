c0da131dc2c962ee3a03608c7b758728293ef088
mvladev <martin.vladev@sap.com>
2017-11-06 08:24:39 +0200

    Change api's group to cluster.k8s.io
    Rationale:
    - tautology - we know that the group will hold API definitions
    - consistency - all other main k8s groups have a single word subdomain:
      `storage.k8s.io`, `networking.k8s.io`
    - bug in code-generator[0] that prevents clientset generation when
      there is a hyphen in the last subdomain of the group
    
    [0]: https://github.com/kubernetes/code-generator/issues/22

--- cluster-api/api/cluster/v1alpha1/doc.go
+++ cluster-api/api/cluster/v1alpha1/doc.go
@@ -17,6 +17,6 @@ limitations under the License.
 // +k8s:deepcopy-gen=package,register
 // +k8s:openapi-gen=false
 
-// +groupName=cluster-api.k8s.io
+// +groupName=cluster.k8s.io
 
 package v1alpha1

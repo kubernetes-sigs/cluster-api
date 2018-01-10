b57733043cc5c57ab4055e15067d74e7433a51ee
Feng Min <fmin@google.com>
2018-01-09 16:36:50 -0800

    Init clusterapi extension api server project.
    `apiserver-boot init repo --domain cluster.k8s.io`
    
    - copy vendored dependencies from the apiserver-builder release
    - create main.go for apiserver and controller-manager
    - create go packages for APIs and controllers
    - create go packages for docs
    - create bazel WORKSPACE file

---
+++ ext-apiserver/cmd/controller-manager/main.go
@@ -0,0 +1,44 @@
+
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	controllerlib "github.com/kubernetes-incubator/apiserver-builder/pkg/controller"
+
+	"k8s.io/kube-deploy/ext-apiserver/pkg/controller"
+)
+
+var kubeconfig = flag.String("kubeconfig", "", "path to kubeconfig")
+
+func main() {
+	flag.Parse()
+	config, err := controllerlib.GetConfig(*kubeconfig)
+	if err != nil {
+		log.Fatalf("Could not create Config for talking to the apiserver: %v", err)
+	}
+
+	controllers, _ := controller.GetAllControllers(config)
+	controllerlib.StartControllerManager(controllers...)
+
+	// Blockforever
+	select {}
+}

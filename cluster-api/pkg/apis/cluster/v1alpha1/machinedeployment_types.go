2d01a3c584f632762ff559b05f8b378ab96de435
Maisem Ali <maisem@google.com>
2018-03-22 11:15:19 -0700

    Introducing MachineDeployment types.

---
+++ cluster-api/pkg/apis/cluster/v1alpha1/machinedeployment_types.go
@@ -0,0 +1,288 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"log"
+
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/endpoints/request"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1validation "k8s.io/apimachinery/pkg/apis/meta/v1/validation"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	utilvalidation "k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	"k8s.io/kube-deploy/cluster-api/pkg/apis/cluster"
+	"k8s.io/kube-deploy/cluster-api/pkg/apis/cluster/common"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// MachineDeployment
+// +k8s:openapi-gen=true
+// +resource:path=machinedeployments,strategy=MachineDeploymentValidationStrategy
+type MachineDeployment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MachineDeploymentSpec   `json:"spec,omitempty"`
+	Status MachineDeploymentStatus `json:"status,omitempty"`
+}
+
+// MachineDeploymentSpec defines the desired state of MachineDeployment
+type MachineDeploymentSpec struct {
+	// Number of desired machines. Defaults to 1.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	Replicas *int32 `json:"replicas,omitempty"`
+
+	// Label selector for machines. Existing MachineSets whose machines are
+	// selected by this will be the ones affected by this deployment.
+	// It must match the machine template's labels.
+	Selector metav1.LabelSelector `json:"selector"`
+
+	// Template describes the machines that will be created.
+	Template MachineTemplateSpec `json:"template"`
+
+	// The deployment strategy to use to replace existing machines with
+	// new ones.
+	// +optional
+	Strategy MachineDeploymentStrategy `json:"strategy,omitempty"`
+
+	// Minimum number of seconds for which a newly created machine should
+	// be ready.
+	// Defaults to 0 (machine will be considered available as soon as it
+	// is ready)
+	// +optional
+	MinReadySeconds *int32 `json:"minReadySeconds,omitempty"`
+
+	// The number of old MachineSets to retain to allow rollback.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// Defaults to 1.
+	// +optional
+	RevisionHistoryLimit *int32 `json:"revisionHistoryLimit,omitempty"`
+
+	// Indicates that the deployment is paused.
+	// +optional
+	Paused bool `json:"paused,omitempty"`
+
+	// The maximum time in seconds for a deployment to make progress before it
+	// is considered to be failed. The deployment controller will continue to
+	// process failed deployments and a condition with a ProgressDeadlineExceeded
+	// reason will be surfaced in the deployment status. Note that progress will
+	// not be estimated during the time a deployment is paused. Defaults to 600s.
+	ProgressDeadlineSeconds *int32 `json:"progressDeadlineSeconds,omitempty"`
+}
+
+// MachineDeploymentStrategy describes how to replace existing machines
+// with new ones.
+type MachineDeploymentStrategy struct {
+	// Type of deployment. Can be only be "RollingUpdate".
+	// Default is RollingUpdate.
+	// +optional
+	Type common.MachineDeploymentStrategyType `json:"type,omitempty"`
+
+	// Rolling update config params. Present only if
+	// MachineDeploymentStrategyType = RollingUpdate.
+	// +optional
+	RollingUpdate *MachineRollingUpdateDeployment `json:"rollingUpdate,omitempty"`
+}
+
+// Spec to control the desired behavior of rolling update.
+type MachineRollingUpdateDeployment struct {
+	// The maximum number of machines that can be unavailable during the update.
+	// Value can be an absolute number (ex: 5) or a percentage of desired
+	// machines (ex: 10%).
+	// Absolute number is calculated from percentage by rounding down.
+	// This can not be 0 if MaxSurge is 0.
+	// Defaults to 0.
+	// Example: when this is set to 30%, the old MachineSet can be scaled
+	// down to 70% of desired machines immediately when the rolling update
+	// starts. Once new machines are ready, old MachineSet can be scaled
+	// down further, followed by scaling up the new MachineSet, ensuring
+	// that the total number of machines available at all times
+	// during the update is at least 70% of desired machines.
+	// +optional
+	MaxUnavailable *intstr.IntOrString `json:"maxUnavailable,omitempty" protobuf:"bytes,1,opt,name=maxUnavailable"`
+
+	// The maximum number of machines that can be scheduled above the
+	// desired number of machines.
+	// Value can be an absolute number (ex: 5) or a percentage of
+	// desired machines (ex: 10%).
+	// This can not be 0 if MaxUnavailable is 0.
+	// Absolute number is calculated from percentage by rounding up.
+	// Defaults to 1.
+	// Example: when this is set to 30%, the new MachineSet can be scaled
+	// up immediately when the rolling update starts, such that the total
+	// number of old and new machines do not exceed 130% of desired
+	// machines. Once old machines have been killed, new MachineSet can
+	// be scaled up further, ensuring that total number of machines running
+	// at any time during the update is at most 130% of desired machines.
+	// +optional
+	MaxSurge *intstr.IntOrString `json:"maxSurge,omitempty" protobuf:"bytes,2,opt,name=maxSurge"`
+}
+
+// MachineDeploymentStatus defines the observed state of MachineDeployment
+type MachineDeploymentStatus struct {
+	// The generation observed by the deployment controller.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty" protobuf:"varint,1,opt,name=observedGeneration"`
+
+	// Total number of non-terminated machines targeted by this deployment
+	// (their labels match the selector).
+	// +optional
+	Replicas int32 `json:"replicas,omitempty" protobuf:"varint,2,opt,name=replicas"`
+
+	// Total number of non-terminated machines targeted by this deployment
+	// that have the desired template spec.
+	// +optional
+	UpdatedReplicas int32 `json:"updatedReplicas,omitempty" protobuf:"varint,3,opt,name=updatedReplicas"`
+
+	// Total number of ready machines targeted by this deployment.
+	// +optional
+	ReadyReplicas int32 `json:"readyReplicas,omitempty" protobuf:"varint,7,opt,name=readyReplicas"`
+
+	// Total number of available machines (ready for at least minReadySeconds)
+	// targeted by this deployment.
+	// +optional
+	AvailableReplicas int32 `json:"availableReplicas,omitempty" protobuf:"varint,4,opt,name=availableReplicas"`
+
+	// Total number of unavailable machines targeted by this deployment.
+	// This is the total number of machines that are still required for
+	// the deployment to have 100% available capacity. They may either
+	// be machines that are running but not yet available or machines
+	// that still have not been created.
+	// +optional
+	UnavailableReplicas int32 `json:"unavailableReplicas,omitempty" protobuf:"varint,5,opt,name=unavailableReplicas"`
+}
+
+// Validate checks that an instance of MachineDeployment is well formed
+func (MachineDeploymentValidationStrategy) Validate(ctx request.Context, obj runtime.Object) field.ErrorList {
+	md := obj.(*cluster.MachineDeployment)
+	log.Printf("Validating fields for MachineDeployment %s\n", md.Name)
+	allErrs := field.ErrorList{}
+
+	allErrs = append(allErrs, ValidateMachineDeploymentSpec(&md.Spec, field.NewPath("spec"))...)
+	return allErrs
+}
+
+func ValidateMachineDeploymentSpec(spec *cluster.MachineDeploymentSpec, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	allErrs = append(allErrs, metav1validation.ValidateLabelSelector(&spec.Selector, fldPath.Child("selector"))...)
+	if len(spec.Selector.MatchLabels)+len(spec.Selector.MatchExpressions) == 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("selector"), spec.Selector, "empty selector is not valid for MachineSet."))
+	}
+	selector, err := metav1.LabelSelectorAsSelector(&spec.Selector)
+	if err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("selector"), spec.Selector, "invalid label selector."))
+	} else {
+		labels := labels.Set(spec.Template.Labels)
+		if !selector.Matches(labels) {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("template", "metadata", "labels"), spec.Template.Labels, "`selector` does not match template `labels`"))
+		}
+	}
+
+	if *spec.Replicas < 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("replicas"), *spec.Replicas, "replicas can not be negative"))
+	}
+
+	allErrs = append(allErrs, ValidateMachineDeploymentStrategy(&spec.Strategy, fldPath.Child("strategy"))...)
+	return allErrs
+}
+
+func ValidateMachineDeploymentStrategy(strategy *cluster.MachineDeploymentStrategy, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	switch strategy.Type {
+	case common.RollingUpdateMachineDeploymentStrategyType:
+		allErrs = append(allErrs, ValidateMachineRollingUpdateDeployment(strategy.RollingUpdate, fldPath.Child("rollingUpdate"))...)
+	}
+	return allErrs
+}
+
+func ValidateMachineRollingUpdateDeployment(rollingUpdate *cluster.MachineRollingUpdateDeployment, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	allErrs = append(allErrs, ValidatePositiveIntOrPercent(rollingUpdate.MaxUnavailable, fldPath.Child("maxUnavailable"))...)
+	allErrs = append(allErrs, ValidatePositiveIntOrPercent(rollingUpdate.MaxSurge, fldPath.Child("maxSurge"))...)
+	maxUnavailable, _ := getIntOrPercent(rollingUpdate.MaxUnavailable, false)
+	maxSurge, _ := getIntOrPercent(rollingUpdate.MaxSurge, true)
+	if maxUnavailable == 0 && maxSurge == 0 {
+		// Both MaxSurge and MaxUnavailable cannot be zero.
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("maxUnavailable"), rollingUpdate.MaxUnavailable, "may not be 0 when `maxSurge` is 0"))
+	}
+
+	// Validate that MaxUnavailable is not more than 100%.
+	if len(utilvalidation.IsValidPercent(rollingUpdate.MaxUnavailable.StrVal)) == 0 && maxUnavailable > 100 {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("maxUnavailable"), rollingUpdate.MaxUnavailable, "should not be more than 100%"))
+	}
+	return allErrs
+}
+
+func ValidatePositiveIntOrPercent(s *intstr.IntOrString, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if x, err := getIntOrPercent(s, false); err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath, s.StrVal, "value should be int(5) or percentage(5%)"))
+	} else if x < 0 {
+		allErrs = append(allErrs, field.Invalid(fldPath, x, "value should not be negative"))
+	}
+	return allErrs
+}
+
+func getIntOrPercent(s *intstr.IntOrString, roundUp bool) (int, error) {
+	return intstr.GetValueFromIntOrPercent(s, 100, roundUp)
+}
+
+// DefaultingFunction sets default MachineDeployment field values
+func (MachineDeploymentSchemeFns) DefaultingFunction(o interface{}) {
+	obj := o.(*MachineDeployment)
+	// set default field values here
+	log.Printf("Defaulting fields for MachineDeployment %s\n", obj.Name)
+	if obj.Spec.Replicas == nil {
+		obj.Spec.Replicas = new(int32)
+		*obj.Spec.Replicas = 1
+	}
+
+	if obj.Spec.ProgressDeadlineSeconds == nil {
+		obj.Spec.ProgressDeadlineSeconds = new(int32)
+		*obj.Spec.ProgressDeadlineSeconds = 600
+	}
+
+	if obj.Spec.RevisionHistoryLimit == nil {
+		obj.Spec.RevisionHistoryLimit = new(int32)
+		*obj.Spec.RevisionHistoryLimit = 1
+	}
+
+	if obj.Spec.Strategy.RollingUpdate == nil {
+		obj.Spec.Strategy.RollingUpdate = &MachineRollingUpdateDeployment{}
+	}
+
+	if obj.Spec.Strategy.RollingUpdate.MaxSurge == nil {
+		x := intstr.FromInt(1)
+		obj.Spec.Strategy.RollingUpdate.MaxSurge = &x
+	}
+
+	if obj.Spec.Strategy.RollingUpdate.MaxUnavailable == nil {
+		x := intstr.FromInt(0)
+		obj.Spec.Strategy.RollingUpdate.MaxUnavailable = &x
+	}
+
+	if len(obj.Namespace) == 0 {
+		obj.Namespace = metav1.NamespaceDefault
+	}
+}

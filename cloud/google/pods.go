3ff99ac48dfdc0bc2a222dd6ba99e6f4b0995519
k4leung4 <kleung@google.com>
2018-05-03 11:54:51 -0700

    Bump version for controller-manager to pick up #108 (#127)
    The pull is for machine/machineset owner refs

--- cloud/google/pods.go
+++ cloud/google/pods.go
@@ -33,7 +33,7 @@ import (
 )
 
 var apiServerImage = "gcr.io/k8s-cluster-api/cluster-apiserver:0.0.2"
-var controllerManagerImage = "gcr.io/k8s-cluster-api/controller-manager:0.0.1"
+var controllerManagerImage = "gcr.io/k8s-cluster-api/controller-manager:0.0.2"
 var machineControllerImage = "gcr.io/k8s-cluster-api/gce-machine-controller:0.0.6"
 
 func init() {

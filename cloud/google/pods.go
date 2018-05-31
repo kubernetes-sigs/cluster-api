7565d2e1e9b2129f7d60efe93ee2240fc4ed1fb1
Kenny Leung <kleung@google.com>
2018-05-31 10:33:42 -0700

    Rev the controller manager version to pick up machine deployment controller
    This is to pick up PR #143, which allows for the creation of machine
    deployments. Rolling update should now work.
    
    Update apiserver for vsphere as that is what is being used for gce and
    terraform.

--- cloud/google/pods.go
+++ cloud/google/pods.go
@@ -33,7 +33,7 @@ import (
 )
 
 var apiServerImage = "gcr.io/k8s-cluster-api/cluster-apiserver:0.0.5"
-var controllerManagerImage = "gcr.io/k8s-cluster-api/controller-manager:0.0.5"
+var controllerManagerImage = "gcr.io/k8s-cluster-api/controller-manager:0.0.6"
 var machineControllerImage = "gcr.io/k8s-cluster-api/gce-machine-controller:0.0.12"
 
 func init() {

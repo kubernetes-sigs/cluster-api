5ce30fc69be44657b35bbaa611136be767d6561a
Robert Bailey <robertbailey@google.com>
2018-06-18 23:28:12 -0700

    Add copyright to the generate scripts and enable better error checking.

--- clusterctl/examples/google/generate-yaml.sh
+++ clusterctl/examples/google/generate-yaml.sh
@@ -1,5 +1,22 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+
+# Copyright 2018 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 GCLOUD_PROJECT=$(gcloud config get-value project)
 ZONE=$(gcloud config get-value compute/zone)
@@ -72,12 +89,12 @@ while test $# -gt 0; do
 done
 
 if [ $OVERWRITE -ne 1 ] && [ -f $MACHINE_GENERATED_FILE ]; then
-  echo File $PROVIDERCOMPONENT_GENERATED_FILE already exists. Delete it manually before running this script.
+  echo File $MACHINE_GENERATED_FILE already exists. Delete it manually before running this script.
   exit 1
 fi
 
 if [ $OVERWRITE -ne 1 ] && [ -f $CLUSTER_GENERATED_FILE ]; then
-  echo File $PROVIDERCOMPONENT_GENERATED_FILE already exists. Delete it manually before running this script.
+  echo File $CLUSTER_GENERATED_FILE already exists. Delete it manually before running this script.
   exit 1
 fi
 

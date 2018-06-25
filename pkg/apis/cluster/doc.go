90d79ea06ae5819e7132f15c6c431a5b6472f5d9
Daniel Lipovetsky <dlipovetsky@users.noreply.github.com>
2018-06-25 15:19:22 -0700

    Generate api and deepcopy for unversioned cluster API (#394)

--- pkg/apis/cluster/doc.go
+++ pkg/apis/cluster/doc.go
@@ -14,11 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
-
 // +k8s:deepcopy-gen=package,register
 // +groupName=cluster.k8s.io
 
 // Package api is the internal version of the API.
 package cluster
-

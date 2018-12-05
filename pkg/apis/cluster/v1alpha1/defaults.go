3c19a662ef90896f5fe42086a3ff4d9a8ff48b79
Justin SB <justinsb@google.com>
2018-12-04 22:49:33 -0500

    Sync defaulting with old code
    Syncing to f31486484d5b33c785540eeaffd47fe57832aef5

--- pkg/apis/cluster/v1alpha1/defaults.go
+++ pkg/apis/cluster/v1alpha1/defaults.go
@@ -17,18 +17,43 @@ limitations under the License.
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/cluster-api/pkg/apis/cluster/common"
 )
 
+// PopulateDefaultsMachineDeployment fills in default field values
+// Currently it is called after reading objects, but it could be called in an admission webhook also
 func PopulateDefaultsMachineDeployment(d *MachineDeployment) {
+	if d.Spec.Replicas == nil {
+		d.Spec.Replicas = new(int32)
+		*d.Spec.Replicas = 1
+	}
+
+	if d.Spec.MinReadySeconds == nil {
+		d.Spec.MinReadySeconds = new(int32)
+		*d.Spec.MinReadySeconds = 0
+	}
+
+	if d.Spec.RevisionHistoryLimit == nil {
+		d.Spec.RevisionHistoryLimit = new(int32)
+		*d.Spec.RevisionHistoryLimit = 1
+	}
+
+	if d.Spec.ProgressDeadlineSeconds == nil {
+		d.Spec.ProgressDeadlineSeconds = new(int32)
+		*d.Spec.ProgressDeadlineSeconds = 600
+	}
+
 	if d.Spec.Strategy == nil {
 		d.Spec.Strategy = &MachineDeploymentStrategy{}
 	}
+
 	if d.Spec.Strategy.Type == "" {
 		d.Spec.Strategy.Type = common.RollingUpdateMachineDeploymentStrategyType
 	}
 
+	// Default RollingUpdate strategy only if strategy type is RollingUpdate.
 	if d.Spec.Strategy.Type == common.RollingUpdateMachineDeploymentStrategyType {
 		if d.Spec.Strategy.RollingUpdate == nil {
 			d.Spec.Strategy.RollingUpdate = &MachineRollingUpdateDeployment{}
@@ -43,8 +68,7 @@ func PopulateDefaultsMachineDeployment(d *MachineDeployment) {
 		}
 	}
 
-	if d.Spec.MinReadySeconds == nil {
-		v := int32(0)
-		d.Spec.MinReadySeconds = &v
+	if len(d.Namespace) == 0 {
+		d.Namespace = metav1.NamespaceDefault
 	}
 }

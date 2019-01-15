750b02221cd5c61afaf86d4a6f8e69c37266a681
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2019-01-16 00:36:46 +0100

    Add JoinClusterTimeoutMachineError (#681)
    * Add JoinClusterTimeoutMachineError
    
    * Add a sample use case to JoinClusterTimeoutMachineError

--- pkg/apis/cluster/common/consts.go
+++ pkg/apis/cluster/common/consts.go
@@ -58,6 +58,15 @@ const (
 	//
 	// Example: cannot resolve EC2 IP address.
 	DeleteMachineError MachineStatusError = "DeleteError"
+
+	// This error indicates that the machine did not join the cluster
+	// as a new node within the expected timeframe after instance
+	// creation at the provider succeeded
+	//
+	// Example use case: A controller that deletes Machines which do
+	// not result in a Node joining the cluster within a given timeout
+	// and that are managed by a MachineSet
+	JoinClusterTimeoutMachineError = "JoinClusterTimeoutError"
 )
 
 type ClusterStatusError string

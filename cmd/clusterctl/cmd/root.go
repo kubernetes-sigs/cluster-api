ee38e8a3e04de08a406aa820fcf57e083a084257
Vince Prignano <vince@vincepri.com>
2019-01-25 08:25:41 -0800

    Update controller-{runtime, tools} / Kubernetes 1.13 (#709)
    Signed-off-by: Vince Prignano <vincepri@vmware.com>

--- cmd/clusterctl/cmd/root.go
+++ cmd/clusterctl/cmd/root.go
@@ -22,7 +22,6 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"k8s.io/klog"
 )
 
@@ -30,24 +29,6 @@ var RootCmd = &cobra.Command{
 	Use:   "clusterctl",
 	Short: "cluster management",
 	Long:  `Simple kubernetes cluster management`,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		// Glog requires this otherwise it complains.
-		flag.CommandLine.Parse(nil)
-
-		// This is a temporary hack to enable proper logging until upstream dependencies
-		// are migrated to fully utilize klog instead of glog.
-		klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
-		klog.InitFlags(klogFlags)
-
-		// Sync the glog and klog flags.
-		cmd.Flags().VisitAll(func(f1 *pflag.Flag) {
-			f2 := klogFlags.Lookup(f1.Name)
-			if f2 != nil {
-				value := f1.Value.String()
-				f2.Value.Set(value)
-			}
-		})
-	},
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 		cmd.Help()
@@ -68,6 +49,7 @@ func exitWithHelp(cmd *cobra.Command, err string) {
 }
 
 func init() {
+	klog.InitFlags(flag.CommandLine)
 	RootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	InitLogs()
 }

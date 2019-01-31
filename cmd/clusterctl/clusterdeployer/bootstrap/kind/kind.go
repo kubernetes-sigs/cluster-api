e962f193351eeb00458cfaa18c9f8b20748f610f
Yoan Blanc <yoan.blanc@exoscale.ch>
2019-01-31 16:50:11 +0100

    fixup! clusterctl: kind should ignore some extra flags
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- cmd/clusterctl/clusterdeployer/bootstrap/kind/kind.go
+++ cmd/clusterctl/clusterdeployer/bootstrap/kind/kind.go
@@ -67,9 +67,8 @@ var execFunc = func(args ...string) (string, error) {
 
 func (k *Kind) Create() error {
 	args := []string{"create", "cluster"}
-	for _, opt := range k.options {
-		args = append(args, fmt.Sprintf("--%v", opt))
-	}
+
+	args = k.appendOptions(args)
 
 	_, err := k.exec(args...)
 	return err
@@ -78,15 +77,7 @@ func (k *Kind) Create() error {
 func (k *Kind) Delete() error {
 	args := []string{"delete", "cluster"}
 
-outer:
-	for _, opt := range k.options {
-		for _, ignoredOption := range ignoredOptions {
-			if strings.HasPrefix(opt, ignoredOption) {
-				continue outer
-			}
-		}
-		args = append(args, fmt.Sprintf("--%v", opt))
-	}
+	args = k.appendOptions(args, ignoredOptions...)
 
 	_, err := k.exec(args...)
 	return err
@@ -109,15 +100,7 @@ func (k *Kind) GetKubeconfig() (string, error) {
 func (k *Kind) getKubeConfigPath() (string, error) {
 	args := []string{"get", "kubeconfig-path"}
 
-outer:
-	for _, opt := range k.options {
-		for _, ignoredOption := range ignoredOptions {
-			if strings.HasPrefix(opt, ignoredOption) {
-				continue outer
-			}
-		}
-		args = append(args, fmt.Sprintf("--%v", opt))
-	}
+	args = k.appendOptions(args, ignoredOptions...)
 
 	out, err := k.exec(args...)
 	if err != nil {
@@ -129,4 +112,19 @@ outer:
 
 func (k *Kind) exec(args ...string) (string, error) {
 	return k.execFunc(args...)
+}
+
+// appendOptions enriches the args with all options but the ignored ones
+func (k *Kind) appendOptions(args []string, ignoredOptions ...string) []string {
+outer:
+	for _, opt := range k.options {
+		for _, ignoredOption := range ignoredOptions {
+			if strings.HasPrefix(opt, ignoredOption) {
+				continue outer
+			}
+		}
+		args = append(args, fmt.Sprintf("--%v", opt))
+	}
+
+	return args
 }

ef592ee13e753b1b032f32faf1930c1e3162ac6a
Benjamin Elder <bentheelder@google.com>
2019-04-01 07:40:34 -0700

    use gnu sed on all platforms (#867)

---
+++ hack/sed.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# this script is a drop in wrapper around gnu-sed that will attempt to only
+# use gnu sed (as opposed to E.G. the default sed on macOS)
+
+# darwin is great
+SED="sed"
+if command -v gsed >/dev/null 2>&1; then
+  SED="gsed"
+fi
+if ! (${SED} --version 2>&1 | grep -q GNU); then
+  echo "!!! GNU sed is required.  If on OS X, use 'brew install gnu-sed'." >&2
+  exit 1
+fi
+
+"${SED}" "$@"

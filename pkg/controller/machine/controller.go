a7df35547823323913f2a92bc0ed32f3d4fe90e1
Michael Gugino <mgugino@redhat.com>
2019-05-22 17:37:45 -0400

    UPSTREAM: <carry>: openshift: Ensure delete continues w/o node
    This commit ensures we can continue with the instance
    and machine deletion in the case that the node
    referenced by the machine is already deleted.
    
    Nodes can potentially be deleted by administrators
    or possibly in previous Reconcile loop of the
    machine-controller that failed to properly update
    annotations.
    
    This commit also adds logging to be more uniform
    with how we log errors elsewhere in the controller.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1713105

--- pkg/controller/machine/controller.go
+++ pkg/controller/machine/controller.go
@@ -199,6 +199,7 @@ func (r *ReconcileMachine) Reconcile(request reconcile.Request) (reconcile.Resul
 		// deleted without a manual intervention.
 		if _, exists := m.ObjectMeta.Annotations[ExcludeNodeDrainingAnnotation]; !exists && m.Status.NodeRef != nil {
 			if err := r.drainNode(m); err != nil {
+				klog.Errorf("Failed to drain node for machine %q: %v", name, err)
 				return delayIfRequeueAfterError(err)
 			}
 		}
@@ -259,6 +260,11 @@ func (r *ReconcileMachine) drainNode(machine *machinev1.Machine) error {
 	}
 	node, err := kubeClient.CoreV1().Nodes().Get(machine.Status.NodeRef.Name, metav1.GetOptions{})
 	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If an admin deletes the node directly, we'll end up here.
+			klog.Infof("Could not find node from noderef, it may have already been deleted: %v", machine.Status.NodeRef.Name)
+			return nil
+		}
 		return fmt.Errorf("unable to get node %q: %v", machine.Status.NodeRef.Name, err)
 	}
 

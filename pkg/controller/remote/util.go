65975e04657978b5879649f4809e9f60fc18704d
Vince Prignano <vincepri@vmware.com>
2019-06-10 14:17:24 -0700

    Add remote/util.go helpers to work with KubeConfig Secrets
    Signed-off-by: Vince Prignano <vincepri@vmware.com>

---
+++ pkg/controller/remote/util.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package remote
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	kubeconfigSecretKey = "value"
+)
+
+// KubeConfigSecretName generates the expected name for the Kubeconfig secret
+// to access a remote cluster given the cluster's name.
+func KubeConfigSecretName(cluster string) string {
+	return fmt.Sprintf("%s-kubeconfig", cluster)
+}
+
+// GetKubeConfigSecret retrieves the KubeConfig Secret (if any)
+// from the given cluster name and namespace.
+func GetKubeConfigSecret(c client.Client, cluster, namespace string) (*corev1.Secret, error) {
+	secret := &corev1.Secret{}
+	secretKey := client.ObjectKey{
+		Namespace: namespace,
+		Name:      KubeConfigSecretName(cluster),
+	}
+
+	if err := c.Get(context.TODO(), secretKey, secret); err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+}
+
+// DecodeKubeConfigSecret uses the Secret to retrieve and decode the data.
+func DecodeKubeConfigSecret(secret *corev1.Secret) ([]byte, error) {
+	encodedKubeconfig, ok := secret.Data[kubeconfigSecretKey]
+	if !ok {
+		return nil, errors.Errorf("missing value in secret %s/%s", secret.Namespace, secret.Name)
+	}
+
+	kubeconfig, err := base64.StdEncoding.DecodeString(string(encodedKubeconfig))
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot decode kubeconfig secret %s/%s", secret.Namespace, secret.Name)
+	}
+
+	return kubeconfig, nil
+}

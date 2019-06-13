e03517671c55ada50e3fd5949853694d6e7c6ec4
Vince Prignano <vincepri@vmware.com>
2019-06-13 10:44:19 -0700

    Update boilerplate check and generated files (#1010)
    Signed-off-by: Vince Prignano <vincepri@vmware.com>
    (cherry picked from commit 0b215f4d102edd23977f04383eb8d2b028bd315b)

--- hack/boilerplate/test/fail.py
+++ hack/boilerplate/test/fail.py
@@ -1,4 +1,8 @@
-# Copyright YEAR The Kubernetes Authors.
+#!/usr/bin/env python
+
+# Copyright 2015 The Kubernetes Authors.
+#
+# failed
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,4 +15,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-

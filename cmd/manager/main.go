0fe1ecd4c9098b32eb61ba1caa2e83335737a6dc
Vince Prignano <vincepri@vmware.com>
2019-06-18 13:03:51 -0700

    Use klog in manager and setup controller-runtime logger (#1022)
    Signed-off-by: Vince Prignano <vincepri@vmware.com>
    (cherry picked from commit 0ebf07643f5180359236d3f07b86e9f8035dc235)

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -18,15 +18,16 @@ package main
 
 import (
 	"flag"
-	"log"
 	"time"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/klog"
+	"k8s.io/klog/klogr"
 	"sigs.k8s.io/cluster-api/pkg/apis"
 	"sigs.k8s.io/cluster-api/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
 )
 
@@ -38,39 +39,43 @@ func main() {
 
 	flag.Parse()
 	if *watchNamespace != "" {
-		log.Printf("Watching cluster-api objects only in namespace %q for reconciliation.", *watchNamespace)
-	}
-	log.Printf("Registering Components.")
-	// Get a config to talk to the apiserver
-	cfg, err := config.GetConfig()
-	if err != nil {
-		log.Fatal(err)
+		klog.Infof("Watching cluster-api objects only in namespace %q for reconciliation", *watchNamespace)
 	}
 
-	// Create a new Cmd to provide shared dependencies and start components
+	// Setup controller-runtime logger.
+	log.SetLogger(klogr.New())
+
+	// Get a config to talk to the api-server.
+	cfg, err := config.GetConfig()
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	// Create a new Cmd to provide shared dependencies and start components.
 	syncPeriod := 10 * time.Minute
 	mgr, err := manager.New(cfg, manager.Options{
 		SyncPeriod: &syncPeriod,
 		Namespace:  *watchNamespace,
 	})
+
 	if err != nil {
-		log.Fatal(err)
+		klog.Fatalf("Failed to create new Manager: %v", err)
 	}
 
-	log.Printf("Registering Components.")
+	klog.Info("Registering Components")
 
-	// Setup Scheme for all resources
+	// Setup Scheme for all resources.
 	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		log.Fatal(err)
+		klog.Fatal(err)
 	}
 
-	// Setup all Controllers
+	// Setup all Controllers.
 	if err := controller.AddToManager(mgr); err != nil {
-		log.Fatal(err)
+		klog.Fatal(err)
 	}
 
-	log.Printf("Starting the Cmd.")
+	klog.Info("Starting the Cmd")
 
 	// Start the Cmd
-	log.Fatal(mgr.Start(signals.SetupSignalHandler()))
+	klog.Fatal(mgr.Start(signals.SetupSignalHandler()))
 }

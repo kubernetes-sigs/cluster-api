8d71b835e2cdfa865bcb00544141558fee745602
Ashish Amarnath <ashish.amarnath@gmail.com>
2019-07-10 10:56:02 -0700

    revert debug code
    Signed-off-by: Ashish Amarnath <ashish.amarnath@gmail.com>

--- hack/verify-docker-build.sh
+++ hack/verify-docker-build.sh
@@ -23,23 +23,4 @@ source "$(dirname "$0")/utils.sh"
 # check if manager docker image builds
 cd_root_path
 
-# DEBUG CODE BEGIN
-service docker start
-# the service can be started but the docker socket not ready, wait for ready
-WAIT_N=0
-MAX_WAIT=5
-while true; do
-    # docker ps -q should only work if the daemon is ready
-    docker ps -q > /dev/null 2>&1 && break
-    if [[ ${WAIT_N} -lt ${MAX_WAIT} ]]; then
-        WAIT_N=$((WAIT_N+1))
-        echo "Waiting for docker to be ready, sleeping for ${WAIT_N} seconds."
-        sleep ${WAIT_N}
-    else
-        echo "Reached maximum attempts, not waiting any longer..."
-        break
-    fi
-done
-# DEBUG CODE END
-
 docker build --file Dockerfile -t capd-manager:pr-verify .

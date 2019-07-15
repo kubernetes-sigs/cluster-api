8573eefcbd594cdc8f6214f2b9e1bbb046f0ea3d
Chuck Ha <chuckh@vmware.com>
2019-07-15 10:58:49 -0400

    Fix json tags on kubeadm types
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- kubeadm/v1beta2/bootstraptokenstring.go
+++ kubeadm/v1beta2/bootstraptokenstring.go
@@ -31,8 +31,8 @@ import (
 // of view and as an authentication method for the node in the bootstrap phase of
 // "kubeadm join". This token is and should be short-lived
 type BootstrapTokenString struct {
-	ID     string `json:"id"`
-	Secret string `json:"secret"`
+	ID     string `json:"-"`
+	Secret string `json:"-"`
 }
 
 // MarshalJSON implements the json.Marshaler interface.

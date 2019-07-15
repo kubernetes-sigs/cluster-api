d60fb41594e03318c874db44eaa5a8102da285ed
Chuck Ha <chuckh@vmware.com>
2019-07-15 11:53:28 -0400

    fix
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- controllers/kubeadmbootstrapconfig_controller.go
+++ controllers/kubeadmbootstrapconfig_controller.go
@@ -21,14 +21,10 @@ import (
 	"fmt"
 
 	"github.com/go-logr/logr"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	kubeadmv1alpha1 "sigs.k8s.io/cluster-api-bootstrap-provider-kubeadm/api/v1alpha1"
 	clusterapiv1alpha2 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 // KubeadmBootstrapConfigReconciler reconciles a KubeadmBootstrapConfig object
@@ -49,35 +45,11 @@ func (r *KubeadmBootstrapConfigReconciler) Reconcile(req ctrl.Request) (ctrl.Res
 	config := &kubeadmv1alpha1.KubeadmBootstrapConfig{}
 	if err := r.Get(ctx, req.NamespacedName, config); err != nil {
 		log.Error(err, "stacktrace", fmt.Sprintf("%+v", err))
+		return ctrl.Result{}, err
 	}
-
-	// find the first machine reference
-	var machineRef v1.OwnerReference
-	for _, ref := range config.OwnerReferences {
-		if ref.Kind == "Machine" {
-			machineRef = ref
-			break
-		}
-	}
-
-	machine := &clusterapiv1alpha2.Machine{}
-	machineKey := types.NamespacedName{
-		Name:      machineRef.Name,
-		Namespace: config.Namespace, // must be in the same namespace
-	}
-	if err := r.Get(ctx, machineKey, machine); err != nil {
-		log.Error(err, "stacktrace", fmt.Sprintf("%+v", err))
-	}
-
-	cluster := &clusterapiv1alpha2.Cluster{}
-	clusterKey := types.NamespacedName{
-		Name:      machine.ClusterName,
-		Namespace: config.Namespace, // must still be in the same namespace
-	}
-	if err := r.Get(ctx, clusterKey, cluster); err != nil {
-		log.Error(err, "stacktrace", fmt.Sprintf("%+v", err))
-	}
-	log.Info("debug", "cluster", cluster, "machine", machine, "config", config)
+	// This is for the import until this function is implemented.
+	m := &clusterapiv1alpha2.Machine{}
+	log.Info("debug, remove", "machine", m)
 	return ctrl.Result{}, nil
 }
 
@@ -86,32 +58,4 @@ func (r *KubeadmBootstrapConfigReconciler) SetupWithManager(mgr ctrl.Manager) er
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&kubeadmv1alpha1.KubeadmBootstrapConfig{}).
 		Complete(r)
-}
-func (r *KubeadmBootstrapConfigReconciler) Map(object handler.MapObject) []reconcile.Request {
-	log := r.Log.WithValues("ns", object.Meta.GetNamespace(), "name", object.Meta.GetName())
-
-	log.Info("mapping machine to config")
-
-	result := []reconcile.Request{}
-	m := &clusterapiv1alpha2.Machine{}
-	key := client.ObjectKey{Namespace: object.Meta.GetNamespace(), Name: object.Meta.GetName()}
-	if err := r.Client.Get(context.Background(), key, m); err != nil {
-		return result
-	}
-
-	// There is no config reference for this machine, so don't enqueue anything
-	if m.Spec.Bootstrap.ConfigRef == nil {
-		return result
-	}
-
-	// Enqueue the associated config ref (not sure if this is actually what this should do?)
-	if m.Spec.Bootstrap.ConfigRef.Name != "" && m.Spec.Bootstrap.ConfigRef.Namespace != "" {
-		result = append(result, reconcile.Request{
-			NamespacedName: types.NamespacedName{
-				Namespace: m.Spec.Bootstrap.ConfigRef.Namespace,
-				Name:      m.Spec.Bootstrap.ConfigRef.Name,
-			},
-		})
-	}
-	return result
 }

cc275023ba3fb276d7d0c13a7d774579d43b9e08
Chuck Ha <chuckh@vmware.com>
2019-08-01 11:03:53 -0400

    use cluster-api mapfunc
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- controllers/dockermachine_controller.go
+++ controllers/dockermachine_controller.go
@@ -20,14 +20,12 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
 	infrastructurev1alpha1 "sigs.k8s.io/cluster-api-provider-docker/api/v1alpha1"
 	capiv1alpha2 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha2"
+	"sigs.k8s.io/cluster-api/pkg/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
@@ -65,32 +63,8 @@ func (r *DockerMachineReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Watches(
 			&source.Kind{Type: &infrastructurev1alpha1.DockerMachine{}},
 			&handler.EnqueueRequestsFromMapFunc{
-				ToRequests: MachineToDockerMachine(capiv1alpha2.SchemeGroupVersion.WithKind("Machine")),
+				ToRequests: util.MachineToInfrastructureMapFunc(capiv1alpha2.SchemeGroupVersion.WithKind("Machine")),
 			},
 		).
 		Complete(r)
-}
-
-// MachineToDockerMachine watches v1alpha2 machines and enqueues associated DockerMachines
-func MachineToDockerMachine(gvk schema.GroupVersionKind) handler.ToRequestsFunc {
-	return func(mo handler.MapObject) []reconcile.Request {
-		m, ok := mo.Object.(*capiv1alpha2.Machine)
-		if !ok {
-			return nil
-		}
-
-		infraRef := m.Spec.InfrastructureRef
-
-		if gvk != infraRef.GroupVersionKind() {
-			return nil
-		}
-		return []reconcile.Request{
-			{
-				NamespacedName: types.NamespacedName{
-					Namespace: m.Namespace,
-					Name:      m.Spec.InfrastructureRef.Name,
-				},
-			},
-		}
-	}
 }

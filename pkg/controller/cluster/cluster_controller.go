7023b3fe000ccb35a545b9c364615faf463ca3b0
Andy Goldstein <goldsteina@vmware.com>
2019-08-02 09:19:52 -0400

    Cluster: delete children first. (#1225)
    Signed-off-by: Andy Goldstein <goldsteina@vmware.com>

--- pkg/controller/cluster/cluster_controller.go
+++ pkg/controller/cluster/cluster_controller.go
@@ -24,7 +24,9 @@ import (
 
 	"github.com/pkg/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog"
 	"sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha2"
@@ -40,7 +42,13 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-const controllerName = "cluster-controller"
+const (
+	controllerName = "cluster-controller"
+
+	// deleteRequeueAfter is how long to wait before checking again to see if the cluster still has children during
+	// deletion.
+	deleteRequeueAfter = 5 * time.Second
+)
 
 // Add creates a new Cluster Controller and adds it to the Manager with default RBAC.
 // The Manager will set fields on the Controller and Start it when the Manager is Started.
@@ -145,36 +153,124 @@ func (r *ReconcileCluster) Reconcile(request reconcile.Request) (_ reconcile.Res
 	}
 
 	if !cluster.ObjectMeta.DeletionTimestamp.IsZero() {
-		if err := r.isDeleteReady(ctx, cluster); err != nil {
-			if requeueErr, ok := errors.Cause(err).(capierrors.HasRequeueAfterError); ok {
-				klog.Infof("Reconciliation for Cluster %q in namespace %q asked to requeue: %v", cluster.Name, cluster.Namespace, err)
-				return reconcile.Result{Requeue: true, RequeueAfter: requeueErr.GetRequeueAfter()}, nil
-			}
-			return reconcile.Result{}, err
-		}
-
-		cluster.ObjectMeta.Finalizers = util.Filter(cluster.ObjectMeta.Finalizers, clusterv1.ClusterFinalizer)
+		return r.reconcileDelete(ctx, cluster)
 	}
 
 	return reconcile.Result{}, nil
 }
 
-// isDeleteReady returns an error if InfrastructureRef referenced object still exist
-// or if any resources linked to this cluster aren't yet deleted.
-func (r *ReconcileCluster) isDeleteReady(ctx context.Context, cluster *v1alpha2.Cluster) error {
-	// TODO(vincepri): List and delete MachineDeployments, MachineSets, Machines, and
-	// block deletion until they're all deleted.
+// reconcileDelete handles cluster deletion.
+// TODO(ncdc): consolidate all deletion logic in here.
+func (r *ReconcileCluster) reconcileDelete(ctx context.Context, cluster *v1alpha2.Cluster) (reconcile.Result, error) {
+	children, err := r.listChildren(ctx, cluster)
+	if err != nil {
+		klog.Errorf("Failed to list children of cluster %s/%s: %v", cluster.Namespace, cluster.Name, err)
+		return reconcile.Result{}, err
+	}
+
+	if len(children) > 0 {
+		klog.Infof("Cluster %s/%s still has %d children - deleting them first", cluster.Namespace, cluster.Name, len(children))
+
+		var errs []error
+
+		for _, child := range children {
+			accessor, err := meta.Accessor(child)
+			if err != nil {
+				klog.Errorf("Cluster %s/%s: couldn't create accessor for type %T: %v", cluster.Namespace, cluster.Name, child, err)
+				continue
+			}
+
+			gvk := child.GetObjectKind().GroupVersionKind().String()
+
+			klog.Infof("Cluster %s/%s: deleting child %s %s", cluster.Namespace, cluster.Name, gvk, accessor.GetName())
+			if err := r.Delete(context.Background(), child); err != nil {
+				err = errors.Wrapf(err, "error deleting cluster %s/%s: failed to delete %s %s", cluster.Namespace, cluster.Name, gvk, accessor.GetName())
+				klog.Errorf(err.Error())
+				errs = append(errs, err)
+			}
+		}
+
+		if len(errs) > 0 {
+			return reconcile.Result{}, kerrors.NewAggregate(errs)
+		}
+
+		// Requeue so we can check the next time to see if there are still any children left.
+		return reconcile.Result{RequeueAfter: deleteRequeueAfter}, nil
+	}
 
 	if cluster.Spec.InfrastructureRef != nil {
 		_, err := external.Get(r.Client, cluster.Spec.InfrastructureRef, cluster.Namespace)
-		if err != nil && !apierrors.IsNotFound(err) {
-			return errors.Wrapf(err, "failed to get %s %q for Cluster %q in namespace %q",
+		switch {
+		case apierrors.IsNotFound(err):
+			// All good - the infra resource has been deleted
+		case err != nil:
+			return reconcile.Result{}, errors.Wrapf(err, "failed to get %s %q for Cluster %s/%s",
 				path.Join(cluster.Spec.InfrastructureRef.APIVersion, cluster.Spec.InfrastructureRef.Kind),
-				cluster.Spec.InfrastructureRef.Name, cluster.Name, cluster.Namespace)
-		} else if err == nil {
-			return &capierrors.RequeueAfterError{RequeueAfter: 10 * time.Second}
+				cluster.Spec.InfrastructureRef.Name, cluster.Namespace, cluster.Name)
+		default:
+			// The infra resource still exists. Once it's been deleted, the cluster will get processed again.
+			// Return here so we don't remove the finalizer yet.
+			return reconcile.Result{}, nil
 		}
 	}
 
-	return nil
+	cluster.Finalizers = util.Filter(cluster.Finalizers, clusterv1.ClusterFinalizer)
+
+	return reconcile.Result{}, nil
+}
+
+// listChildren returns a list of MachineDeployments, MachineSets, and Machines than have an owner reference to cluster
+func (r *ReconcileCluster) listChildren(ctx context.Context, cluster *v1alpha2.Cluster) ([]runtime.Object, error) {
+	listOptions := []client.ListOptionFunc{
+		client.InNamespace(cluster.Namespace),
+		client.MatchingLabels(map[string]string{clusterv1.MachineClusterLabelName: cluster.Name}),
+	}
+
+	machineDeployments := &clusterv1.MachineDeploymentList{}
+	if err := r.Client.List(ctx, machineDeployments, listOptions...); err != nil {
+		return nil, errors.Wrapf(err, "failed to list MachineDeployments for cluster %s/%s", cluster.Namespace, cluster.Name)
+	}
+
+	machineSets := &clusterv1.MachineSetList{}
+	if err := r.Client.List(ctx, machineSets, listOptions...); err != nil {
+		return nil, errors.Wrapf(err, "failed to list MachineSets for cluster %s/%s", cluster.Namespace, cluster.Name)
+	}
+
+	machines := &clusterv1.MachineList{}
+	if err := r.Client.List(ctx, machines, listOptions...); err != nil {
+		return nil, errors.Wrapf(err, "failed to list Machines for cluster %s/%s", cluster.Namespace, cluster.Name)
+	}
+
+	var children []runtime.Object
+	eachFunc := func(o runtime.Object) error {
+		acc, err := meta.Accessor(o)
+		if err != nil {
+			klog.Errorf("Cluster %s/%s: couldn't create accessor for type %T: %v", cluster.Namespace, cluster.Name, o, err)
+			return nil
+		}
+
+		if !acc.GetDeletionTimestamp().IsZero() {
+			// Don't add deleted children
+			return nil
+		}
+
+		if util.PointsTo(acc.GetOwnerReferences(), &cluster.ObjectMeta) {
+			children = append(children, o)
+		}
+
+		return nil
+	}
+
+	lists := map[string]runtime.Object{
+		"MachineDeployment": machineDeployments,
+		"MachineSet":        machineSets,
+		"Machine":           machines,
+	}
+	for name, list := range lists {
+		if err := meta.EachListItem(list, eachFunc); err != nil {
+			return nil, errors.Wrapf(err, "error finding %s children of cluster %s/%s", name, cluster.Namespace, cluster.Name)
+		}
+	}
+
+	return children, nil
 }

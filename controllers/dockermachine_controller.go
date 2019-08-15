cf38588b388b34e3a33f3378ac8fa053a614d8d6
Chuck Ha <chuckh@vmware.com>
2019-08-15 15:35:58 -0400

    Clean up controllers
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- controllers/dockermachine_controller.go
+++ controllers/dockermachine_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -65,9 +66,29 @@ func (r *DockerMachineReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, re
 		return ctrl.Result{}, err
 	}
 
+	// Get the Cluster API Machine
+	machine, err := util.GetOwnerMachine(ctx, r.Client, dockerMachine.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if machine == nil {
+		log.Info("Waiting for Machine Controller to set OwnerRef on DockerMachine")
+		return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+	}
+
+	// Get the Cluster API Cluster
+	cluster, err := util.GetClusterFromMetadata(ctx, r.Client, machine.ObjectMeta)
+	if err != nil {
+		log.Error(err, "Failed to get cluster for docker machine")
+		return ctrl.Result{}, err
+	}
+	if cluster == nil {
+		log.Info(fmt.Sprintf("Please associate this machine with a cluster using the label %s: <name of cluster>", capiv1alpha2.MachineClusterLabelName))
+		return ctrl.Result{RequeueAfter: 30 * time.Second}, nil
+	}
+
 	// Store Docker Machine early state to allow patching.
 	patch := client.MergeFrom(dockerMachine.DeepCopy())
-
 	defer func() {
 		if err := r.patchMachine(ctx, dockerMachine, patch); err != nil {
 			r.Log.Error(err, "Error Patching DockerMachine", "name", dockerMachine.GetName())
@@ -77,45 +98,48 @@ func (r *DockerMachineReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, re
 		}
 	}()
 
-	// Get the cluster api machine
-	machine, err := util.GetOwnerMachine(ctx, r.Client, dockerMachine.ObjectMeta)
-
-	if err != nil {
-		return ctrl.Result{}, nil
-	}
-	if machine == nil {
-		log.Info("Waiting for Machine Controller to set OwnerRef on DockerMachine")
-		return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
-	}
-
-	// Get the cluster
-	cluster, err := util.GetClusterFromMetadata(ctx, r.Client, machine.ObjectMeta)
-	if err != nil {
-		log.Error(err, "failed to get cluster")
-	}
-
 	// If the DockerMachine doesn't have finalizer, add it.
 	if !util.Contains(dockerMachine.Finalizers, capiv1alpha2.MachineFinalizer) {
 		dockerMachine.Finalizers = append(dockerMachine.Finalizers, infrastructurev1alpha2.MachineFinalizer)
 	}
 
-	//reconcileDelete dockerMachine
+	// If this is the delete case, delete the container and remove the finalizer
 	if !machine.ObjectMeta.DeletionTimestamp.IsZero() {
 		log.Info("Start reconcileDelete dockerMachine")
 		return r.reconcileDelete(ctx, cluster, machine, dockerMachine)
 	}
 
-	// create docker node
-	if dockerMachine.Spec.ProviderID != nil {
+	// If the machine already has a ProviderID then there is no action to take
+	if machine.Spec.ProviderID != nil {
 		return ctrl.Result{}, nil
 	}
 
-	result, err := r.create(ctx, cluster, machine, dockerMachine)
+	// If there is no provider id and no bootstrap data we are waiting for the bootstrap data
+	if machine.Spec.Bootstrap.Data == nil {
+		log.Info("Waiting for machine bootstrap")
+		return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+	}
+
+	// Ensuring cluster is ready for joining
+	clusterExists, err := kindcluster.IsKnown(cluster.Name)
 	if err != nil {
+		log.Error(err, "Error finding cluster-name", "cluster", cluster.Name)
 		return ctrl.Result{}, err
 	}
 
-	return result, nil
+	// If there's no cluster, requeue the request until there is one
+	if !clusterExists {
+		r.Log.Info("There is no cluster yet, waiting for a cluster before creating machines")
+		return ctrl.Result{RequeueAfter: time.Second * 30}, nil
+	}
+
+	// At this point the machine is not yet provisioned. Create if possible.
+	if machine.Spec.Bootstrap.Data != nil {
+		return r.create(ctx, cluster, machine, dockerMachine)
+	}
+
+	// We are waiting for bootstrap data
+	return ctrl.Result{RequeueAfter: 30 * time.Second}, nil
 }
 
 func getRole(machine *capiv1alpha2.Machine) string {
@@ -135,43 +159,28 @@ func (r *DockerMachineReconciler) create(
 	dockerMachine *infrastructurev1alpha2.DockerMachine) (ctrl.Result, error) {
 
 	log := r.Log.WithValues("cluster", c.Name)
-
 	log.Info("Creating a machine for cluster")
-	clusterExists, err := kindcluster.IsKnown(c.Name)
-	if err != nil {
-		r.Log.Error(err, "Error finding cluster-name", "cluster", c.Name)
-		return ctrl.Result{}, err
-	}
-	// If there's no cluster, requeue the request until there is one
-	if !clusterExists {
-		r.Log.Info("There is no cluster yet, waiting for a cluster before creating machines")
-		return ctrl.Result{RequeueAfter: time.Second * 30}, nil
-	}
-	r.Log.Info("Is there a cluster?", "cluster-exists", clusterExists)
-
-	if machine.Spec.Bootstrap.Data != nil {
-		var node *nodes.Node
-		if isControlPlaneMachine(machine) {
-			r.Log.Info("Adding a control plane node", "machine-name", machine.GetName(), "cluster-name", c.Name)
-			node, err = actions.AddControlPlane(c.Name, machine.GetName(), *machine.Spec.Version)
-			if err != nil {
-				r.Log.Error(err, "Error adding control plane")
-				return ctrl.Result{}, err
-			}
-		} else {
-			r.Log.Info("Creating a new worker node")
-			node, err = actions.AddWorker(c.Name, machine.GetName(), *machine.Spec.Version)
-			if err != nil {
-				r.Log.Error(err, "Error creating new worker node")
-				return ctrl.Result{}, err
-			}
+	var node *nodes.Node
+	var err error
+	if isControlPlaneMachine(machine) {
+		r.Log.Info("Adding a control plane node", "machine-name", machine.GetName(), "cluster-name", c.Name)
+		node, err = actions.AddControlPlane(c.Name, machine.GetName(), *machine.Spec.Version)
+		if err != nil {
+			r.Log.Error(err, "Error adding control plane")
+			return ctrl.Result{}, err
 		}
-		// set the machine's providerID
-		providerID := actions.ProviderID(node.Name())
-		dockerMachine.Spec.ProviderID = &providerID
-		dockerMachine.Status.Ready = true
-		return ctrl.Result{}, nil
+	} else {
+		r.Log.Info("Creating a new worker node")
+		node, err = actions.AddWorker(c.Name, machine.GetName(), *machine.Spec.Version)
+		if err != nil {
+			r.Log.Error(err, "Error creating new worker node")
+			return ctrl.Result{}, err
+		}
 	}
+	// set the machine's providerID
+	providerID := actions.ProviderID(node.Name())
+	dockerMachine.Spec.ProviderID = &providerID
+	dockerMachine.Status.Ready = true
 	return ctrl.Result{}, nil
 }
 
@@ -194,16 +203,17 @@ func (r *DockerMachineReconciler) reconcileDelete(
 	machine *capiv1alpha2.Machine,
 	dockerMachine *infrastructurev1alpha2.DockerMachine,
 ) (ctrl.Result, error) {
-
+	log := r.Log.WithValues("cluster", cluster.Name, "machine", machine.Name)
 	if isControlPlaneMachine(machine) {
-		err := actions.DeleteControlPlane(cluster.GetName(), machine.GetName())
+		err := actions.DeleteControlPlane(cluster.Name, machine.Name)
 		if err != nil {
+			log.Error(err, "Error deleting control-plane machine")
 			return ctrl.Result{RequeueAfter: time.Second * 30}, err
 		}
 	} else {
-		err := actions.DeleteWorker(cluster.GetName(), machine.GetName())
+		err := actions.DeleteWorker(cluster.Name, machine.Name)
 		if err != nil {
-			r.Log.Error(err, "Error deleting worker node", "nodeName", machine.GetName())
+			log.Error(err, "Error deleting worker node")
 			return ctrl.Result{RequeueAfter: time.Second * 30}, nil
 		}
 	}

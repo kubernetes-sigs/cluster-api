a7412c7d967c2af5d0bfbeb9b1895487311daed1
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2020-01-14 11:27:21 -0800

    :bug: Kubeadm-based Control Plane: Fix error handling during cleanup
    If err is nil, the condition evaluates to true. The condition should evaluate
    to true only if there is an actual error.
    
    Signed-off-by: Daniel Lipovetsky <dlipovetsky@d2iq.com>

--- controlplane/kubeadm/controllers/kubeadm_control_plane_controller.go
+++ controlplane/kubeadm/controllers/kubeadm_control_plane_controller.go
@@ -394,7 +394,7 @@ func (r *KubeadmControlPlaneReconciler) cleanupFromGeneration(ctx context.Contex
 			config.SetNamespace(ref.Namespace)
 			config.SetName(ref.Name)
 
-			if err := r.Client.Delete(ctx, config); !apierrors.IsNotFound(err) {
+			if err := r.Client.Delete(ctx, config); err != nil && !apierrors.IsNotFound(err) {
 				errs = append(errs, errors.Wrap(err, "failed to cleanup generated resources after error"))
 			}
 		}

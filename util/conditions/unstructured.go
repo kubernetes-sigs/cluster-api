06f6f808610b9b6d9cb57d128bc020b3425fc5f0
fabriziopandini <fpandini@vmware.com>
2020-05-28 19:31:20 +0200

    Add conditions patch utils

--- util/conditions/unstructured.go
+++ util/conditions/unstructured.go
@@ -16,7 +16,11 @@ limitations under the License.
 package conditions
 
 import (
+	"fmt"
+
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
 	"sigs.k8s.io/cluster-api/util"
 )
@@ -27,11 +31,17 @@ func UnstructuredGetter(u *unstructured.Unstructured) Getter {
 	return &unstructuredWrapper{Unstructured: u}
 }
 
+// UnstructuredSetter return a Setter object that can set conditions from an Unstructured object.
+// Important. This method should be used only with types implementing Cluster API conditions.
+func UnstructuredSetter(u *unstructured.Unstructured) Setter {
+	return &unstructuredWrapper{Unstructured: u}
+}
+
 type unstructuredWrapper struct {
 	*unstructured.Unstructured
 }
 
-// GetConditions returns the list of conditions for an Unstructured object.
+// GetConditions returns the list of conditions from an Unstructured object.
 //
 // NOTE: Due to the constraints of JSON-unmarshal, this operation is to be considered best effort.
 // In more details:
@@ -46,4 +56,22 @@ func (c *unstructuredWrapper) GetConditions() clusterv1.Conditions {
 		return nil
 	}
 	return conditions
+}
+
+// SetConditions set the conditions into an Unstructured object.
+func (c *unstructuredWrapper) SetConditions(conditions clusterv1.Conditions) {
+	v := make([]interface{}, 0, len(conditions))
+	for i := range conditions {
+		m, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&conditions[i])
+		if err != nil {
+			klog.Error(fmt.Sprintf("Failed to convert condition on %s/%s:  %v", c.GetObjectKind().GroupVersionKind().Kind, c.GetName(), err))
+		}
+		v = append(v, m)
+	}
+	// unstructured.SetNestedField returns an error only if value cannot be set because one of
+	// the nesting levels is not a map[string]interface{}; this is not the case so the error should never happen here.
+	err := unstructured.SetNestedField(c.Unstructured.Object, v, "status", "conditions")
+	if err != nil {
+		klog.Error(fmt.Sprintf("Failed to set conditions on %s/%s:  %v", c.GetObjectKind().GroupVersionKind().Kind, c.GetName(), err))
+	}
 }

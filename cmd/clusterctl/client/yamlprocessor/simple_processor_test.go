d4907ef7aeed6711cff4fb41ba7f0da7a0b5542d
Warren Fernandes <warrenf@vmware.com>
2020-06-29 14:16:29 -0600

    Use drone/envsubst to provide defaults
    - This is change to the SimpleProcessor so defaults will be available to
    cluster templates and provider components.
    
    - Create specific errMissingVariables error
    Since we are using a map, the order of the variables in the error message was changing.
    This would make the tests flaky since we were doing a fixed substring comparison.
    So the error which wraps the slice of missing vars is used
    
    - Use a specific sha of drone/envsubst until a tag is cut.

--- cmd/clusterctl/client/yamlprocessor/simple_processor_test.go
+++ cmd/clusterctl/client/yamlprocessor/simple_processor_test.go
@@ -35,9 +35,10 @@ func TestSimpleProcessor_GetVariables(t *testing.T) {
 		data string
 	}
 	tests := []struct {
-		name string
-		args args
-		want []string
+		name    string
+		args    args
+		want    []string
+		wantErr bool
 	}{
 		{
 			name: "variable with different spacing around the name",
@@ -49,7 +50,7 @@ func TestSimpleProcessor_GetVariables(t *testing.T) {
 		{
 			name: "variables used in many places are grouped",
 			args: args{
-				data: "yaml with ${A} ${A} ${A}",
+				data: "yaml with ${A } ${A} ${A}",
 			},
 			want: []string{"A"},
 		},
@@ -68,11 +69,18 @@ func TestSimpleProcessor_GetVariables(t *testing.T) {
 			want: []string{"A", "B", "C"},
 		},
 		{
-			name: "variables with regex metacharacters",
+			name: "returns error for variables with regex metacharacters",
 			args: args{
 				data: "yaml with ${BA$R}\n${FOO}",
 			},
-			want: []string{"BA$R", "FOO"},
+			wantErr: true,
+		},
+		{
+			name: "variables with envsubst functions are properly parsed",
+			args: args{
+				data: "yaml with ${C:=default}\n${B}\n${A=foobar}",
+			},
+			want: []string{"A", "B", "C"},
 		},
 	}
 
@@ -80,8 +88,13 @@ func TestSimpleProcessor_GetVariables(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			g := NewWithT(t)
 			p := NewSimpleProcessor()
-
-			g.Expect(p.GetVariables([]byte(tt.args.data))).To(Equal(tt.want))
+			actual, err := p.GetVariables([]byte(tt.args.data))
+			if tt.wantErr {
+				g.Expect(err).To(HaveOccurred())
+				return
+			}
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(actual).To(Equal(tt.want))
 		})
 	}
 }
@@ -92,35 +105,26 @@ func TestSimpleProcessor_Process(t *testing.T) {
 		configVariablesClient config.VariablesClient
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    []byte
-		wantErr bool
+		name             string
+		args             args
+		want             []byte
+		wantErr          bool
+		missingVariables []string
 	}{
 		{
-			name: "pass and replaces variables",
+			name: "replaces legacy variables names (with spaces)",
 			args: args{
-				yaml: []byte("foo ${ BAR }"),
+				yaml: []byte("foo ${ BAR }, ${BAR }, ${ BAR}"),
 				configVariablesClient: test.NewFakeVariableClient().
 					WithVar("BAR", "bar"),
 			},
-			want:    []byte("foo bar"),
+			want:    []byte("foo bar, bar, bar"),
 			wantErr: false,
 		},
 		{
-			name: "pass and replaces variables when variable name contains regex metacharacters",
+			name: "replaces variables when variable value contains regex metacharacters",
 			args: args{
-				yaml: []byte("foo ${ BA$R }"),
-				configVariablesClient: test.NewFakeVariableClient().
-					WithVar("BA$R", "bar"),
-			},
-			want:    []byte("foo bar"),
-			wantErr: false,
-		},
-		{
-			name: "pass and replaces variables when variable value contains regex metacharacters",
-			args: args{
-				yaml: []byte("foo ${ BAR }"),
+				yaml: []byte("foo ${BAR}"),
 				configVariablesClient: test.NewFakeVariableClient().
 					WithVar("BAR", "ba$r"),
 			},
@@ -128,12 +132,45 @@ func TestSimpleProcessor_Process(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "returns error when missing values for template variables",
+			name: "uses default values if variable doesn't exist in variables client",
 			args: args{
-				yaml:                  []byte("foo ${ BAR } ${ BAZ }"),
-				configVariablesClient: test.NewFakeVariableClient(),
+				yaml: []byte("foo ${BAR=default_bar} ${BAZ:=default_baz} ${CAR=default_car} ${CAZ:-default_caz} ${DAR=default_dar}"),
+				configVariablesClient: test.NewFakeVariableClient().
+					// CAZ,DAR is set but has no value
+					WithVar("BAR", "ba$r").WithVar("CAZ", "").WithVar("DAR", ""),
 			},
-			want:    nil,
+			want:    []byte("foo ba$r default_baz default_car default_caz default_dar"),
+			wantErr: false,
+		},
+		{
+			name: "uses default variables if main variable is doesn't exist",
+			args: args{
+				yaml: []byte("foo ${BAR=default_bar} ${BAZ:=prefix${DEF}-suffix} ${CAZ=${DEF}}"),
+				configVariablesClient: test.NewFakeVariableClient().
+					WithVar("BAR", "ba$r").WithVar("DEF", "football"),
+			},
+			want:    []byte("foo ba$r prefixfootball-suffix football"),
+			wantErr: false,
+		},
+		{
+			name: "returns error with missing template variables listed (for better ux)",
+			args: args{
+				yaml: []byte("foo ${ BAR} ${BAZ} ${CAR}"),
+				configVariablesClient: test.NewFakeVariableClient().
+					WithVar("CAR", "car"),
+			},
+			want:             nil,
+			wantErr:          true,
+			missingVariables: []string{"BAR", "BAZ"},
+		},
+		{
+			name: "returns error when variable name contains regex metacharacters",
+			args: args{
+				yaml: []byte("foo ${BA$R} ${BA_R}"),
+				configVariablesClient: test.NewFakeVariableClient().
+					WithVar("BA$R", "bar").WithVar("BA_R", "ba_r"),
+			},
+			want:    []byte("foo bar ba_r"),
 			wantErr: true,
 		},
 	}
@@ -146,6 +183,14 @@ func TestSimpleProcessor_Process(t *testing.T) {
 			got, err := p.Process(tt.args.yaml, tt.args.configVariablesClient.Get)
 			if tt.wantErr {
 				g.Expect(err).To(HaveOccurred())
+				if len(tt.missingVariables) != 0 {
+					e, ok := err.(*errMissingVariables)
+					g.Expect(ok).To(BeTrue())
+					g.Expect(e.Missing).To(ConsistOf(tt.missingVariables))
+				}
+				// we want to ensure that we keep returning the original yaml
+				// as per the intended behavior of Process
+				g.Expect(got).To(Equal(tt.args.yaml))
 				return
 			}
 			g.Expect(err).NotTo(HaveOccurred())

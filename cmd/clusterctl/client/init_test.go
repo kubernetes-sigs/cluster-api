53485548a95f2fdec0ef81b5546a7a4aa5e7d4da
shysank <sankarans@vmware.com>
2021-01-19 12:04:49 -0800

    :seedling: upgrade kube-rbac-proxy to v0.8.0
    This PR will update kube-rbac-proxy for the release-0.3 branch. This
    helps ensure that the CAPI v0.3.x series can be deployed multi-arch. It is a
    straight backport from the commit mentioned below with the exception of
    fixing merge conflicts around docs.
    
    (cherry picked from commit aa51850504544661eead1685479341bccf9dd39e)
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- cmd/clusterctl/client/init_test.go
+++ cmd/clusterctl/client/init_test.go
@@ -75,7 +75,7 @@ func Test_clusterctlClient_InitImages(t *testing.T) {
 				kubeconfigContext:      "mgmt-context",
 			},
 			expectedImages: []string{
-				"gcr.io/kubebuilder/kube-rbac-proxy:v0.4.1",
+				"gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0",
 				"us.gcr.io/k8s-artifacts-prod/cluster-api-aws/cluster-api-aws-controller:v0.5.3",
 			},
 			wantErr: false,
@@ -894,7 +894,7 @@ spec:
   template:
     spec:
       containers:
-      - image: gcr.io/kubebuilder/kube-rbac-proxy:v0.4.1
+      - image: gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0
         name: kube-rbac-proxy
       - image: us.gcr.io/k8s-artifacts-prod/cluster-api-aws/cluster-api-aws-controller:v0.5.3
         name: manager

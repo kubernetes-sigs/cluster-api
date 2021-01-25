4f2c5d52601578ad1915c1baa16b303cca46ae01
Warren Fernandes <warren.f.fernandes@gmail.com>
2021-01-25 10:49:09 -0700

    Initial scaffolding for operator
    Domain is cluster.x-k8s.io
    
    Create API scaffolding for CoreProvider
    
    kubebuilder create api --group operator --version v1alpha4 --kind CoreProvider
    
    Create API scaffolding for BootstrapProvider
    
    kubebuilder create api --group operator --version v1alpha4 --kind BootstrapProvider
    Created Resources, not controllers
    
    Create API scaffolding for ControlPlaneProvider
    
    kubebuilder create api --group operator --version v1alpha4 --kind ControlPlaneProvider
    Created Resources, not controllers
    
    Create API scaffolding for InfrastructureProvider
    
    kubebuilder create api --group operator --version v1alpha4 --kind InfrastructureProvider
    Created Resources, not controllers
    
    Add placeholders for ProviderSpec and ProviderStatus
    
    Fix some module imports. Will be cleaned up in future PRs.
    
    Fix boilerplate on files
    
    Move operator to exp folder

---
+++ exp/operator/api/v1alpha4/bootstrapprovider_types.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha4
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// BootstrapProviderSpec defines the desired state of BootstrapProvider
+type BootstrapProviderSpec struct {
+	ProviderSpec `json:",inline"`
+}
+
+// BootstrapProviderStatus defines the observed state of BootstrapProvider
+type BootstrapProviderStatus struct {
+	ProviderStatus `json:",inline"`
+}
+
+// +kubebuilder:object:root=true
+
+// BootstrapProvider is the Schema for the bootstrapproviders API
+type BootstrapProvider struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BootstrapProviderSpec   `json:"spec,omitempty"`
+	Status BootstrapProviderStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// BootstrapProviderList contains a list of BootstrapProvider
+type BootstrapProviderList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BootstrapProvider `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BootstrapProvider{}, &BootstrapProviderList{})
+}
---
+++ exp/operator/go.mod
@@ -0,0 +1,14 @@
+module sigs.k8s.io/cluster-api/exp/operator
+
+go 1.13
+
+require (
+	github.com/go-logr/logr v0.1.0
+	github.com/onsi/ginkgo v1.12.1
+	github.com/onsi/gomega v1.10.1
+	k8s.io/apimachinery v0.17.9
+	k8s.io/client-go v0.17.9
+	sigs.k8s.io/cluster-api v0.3.13
+	sigs.k8s.io/controller-runtime v0.5.14
+	sigs.k8s.io/structured-merge-diff v1.0.1-0.20191108220359-b1b620dd3f06 // indirect
+)

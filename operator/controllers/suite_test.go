69f6d63d40953303d053f895588b68e6974b5011
Warren Fernandes <warrenf@vmware.com>
2021-01-27 16:37:52 -0700

    Use klogr intead of zap

--- operator/controllers/suite_test.go
+++ operator/controllers/suite_test.go
@@ -24,11 +24,11 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/klogr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	operatorv1alpha4 "sigs.k8s.io/cluster-api/operator/api/v1alpha4"
 	// +kubebuilder:scaffold:imports
@@ -50,7 +50,7 @@ func TestAPIs(t *testing.T) {
 }
 
 var _ = BeforeSuite(func(done Done) {
-	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+	logf.SetLogger(klogr.New())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{

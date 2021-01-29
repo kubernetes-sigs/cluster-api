a6d13af5a0b77269c7909200105b6f83eec77678
Warren Fernandes <warrenf@vmware.com>
2021-01-29 14:12:36 -0700

    Refactor main.go to be consistent with other controller mains

--- operator/main.go
+++ operator/main.go
@@ -17,12 +17,17 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
+	"math/rand"
 	"os"
+	"time"
 
+	"github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/klog"
 	"k8s.io/klog/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
 
@@ -33,34 +38,48 @@ import (
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme               = runtime.NewScheme()
+	setupLog             = ctrl.Log.WithName("setup")
+	metricsBindAddr      string
+	enableLeaderElection bool
+	webhookPort          int
 )
 
 func init() {
-	_ = clientgoscheme.AddToScheme(scheme)
+	klog.InitFlags(nil)
 
+	_ = clientgoscheme.AddToScheme(scheme)
 	_ = operatorv1alpha4.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
+func InitFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&metricsBindAddr, "metrics-bind-addr", ":8080",
+		"The address the metric endpoint binds to.")
+
+	fs.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+
+	fs.IntVar(&webhookPort, "webhook-port", 0,
+		"Webhook Server port, disabled by default. When enabled, the manager will only work as webhook server, no reconcilers are installed.")
+
+}
+
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	flag.StringVar(&metricsAddr, "metrics-bind-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+
+	InitFlags(pflag.CommandLine)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
 
 	ctrl.SetLogger(klogr.New())
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
+		MetricsBindAddress: metricsBindAddr,
+		Port:               webhookPort,
 		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "83021b55.cluster.x-k8s.io",
+		LeaderElectionID:   "operator-leader-election-capi",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -70,34 +89,50 @@ func main() {
 	// Setup the context that's going to be used in controllers and for the manager.
 	ctx := ctrl.SetupSignalHandler()
 
-	if err = (&controllers.CoreProviderReconciler{
+	setupReconcilers(ctx, mgr)
+	setupWebhooks(mgr)
+
+	// +kubebuilder:scaffold:builder
+	setupLog.Info("starting manager", "version", version.Get().String())
+	if err := mgr.Start(ctx); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
+
+func setupReconcilers(ctx context.Context, mgr ctrl.Manager) {
+	if webhookPort != 0 {
+		return
+	}
+
+	if err := (&controllers.CoreProviderReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "CoreProvider")
 		os.Exit(1)
 	}
-	if err = (&operatorv1alpha4.CoreProvider{}).SetupWebhookWithManager(mgr); err != nil {
+}
+
+func setupWebhooks(mgr ctrl.Manager) {
+	if webhookPort == 0 {
+		return
+	}
+
+	if err := (&operatorv1alpha4.CoreProvider{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "CoreProvider")
 		os.Exit(1)
 	}
-	if err = (&operatorv1alpha4.BootstrapProvider{}).SetupWebhookWithManager(mgr); err != nil {
+	if err := (&operatorv1alpha4.BootstrapProvider{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "BootstrapProvider")
 		os.Exit(1)
 	}
-	if err = (&operatorv1alpha4.ControlPlaneProvider{}).SetupWebhookWithManager(mgr); err != nil {
+	if err := (&operatorv1alpha4.ControlPlaneProvider{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "ControlPlaneProvider")
 		os.Exit(1)
 	}
-	if err = (&operatorv1alpha4.InfrastructureProvider{}).SetupWebhookWithManager(mgr); err != nil {
+	if err := (&operatorv1alpha4.InfrastructureProvider{}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "InfrastructureProvider")
-		os.Exit(1)
-	}
-	// +kubebuilder:scaffold:builder
-
-	setupLog.Info("starting manager", "version", version.Get().String())
-	if err := mgr.Start(ctx); err != nil {
-		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
 }

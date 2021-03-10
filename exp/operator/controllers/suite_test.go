b8e7545611b6034da0bef4867904fe203255c83e
Warren Fernandes <warren.f.fernandes@gmail.com>
2021-03-10 22:27:12 +0100

    Setup operator Makefile
    Rebased and fix version pkg path
    
    Remove zap logger
    
    Remove .gitignore, Dockerfile and modules files
    
    Add context to reconciler arguments
    
    Add test tasks for operator
    
    Add binary building tasks for operator
    
    Add linting tasks for operator
    
    Add generate tasks for operator
    
    Add Docker tasks for operator
    
    Add release command for operator
    
    Add cleanup tasks for operator
    
    Make operator more consistent with other projects

--- exp/operator/controllers/suite_test.go
+++ exp/operator/controllers/suite_test.go
@@ -27,8 +27,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	operatorv1alpha4 "sigs.k8s.io/cluster-api/exp/operator/api/v1alpha4"
 	// +kubebuilder:scaffold:imports
@@ -50,8 +48,6 @@ func TestAPIs(t *testing.T) {
 }
 
 var _ = BeforeSuite(func(done Done) {
-	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
-
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},

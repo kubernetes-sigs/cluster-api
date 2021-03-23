5d9fe420468c3f6e98bb697961bc657bc1ebdf2b
Alexander Demichev <demichev.alexander@gmail.com>
2021-03-23 21:25:29 +0100

    Fix CI errors for operator

--- scripts/ci-verify.sh
+++ scripts/ci-verify.sh
@@ -18,7 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "${REPO_ROOT}" || exit 1
 
 echo "*** Verifying Cluster API ***"

5ad599e4122a074792b1d01a915a049d627e3189
jan-est <jan.tilles@est.tech>
2021-04-07 11:45:44 +0300

    Backporting rolloutStrategy caused an issue 4437 in release v0.3.15. This PR changes all KCP fields under spec.rolloutStrategy mutable in master branch.

--- controlplane/kubeadm/api/v1alpha4/kubeadm_control_plane_webhook.go
+++ controlplane/kubeadm/api/v1alpha4/kubeadm_control_plane_webhook.go
@@ -136,7 +136,7 @@ func (in *KubeadmControlPlane) ValidateUpdate(old runtime.Object) error {
 		{spec, "version"},
 		{spec, "upgradeAfter"},
 		{spec, "nodeDrainTimeout"},
-		{spec, "rolloutStrategy"},
+		{spec, "rolloutStrategy", "*"},
 	}
 
 	allErrs := in.validateCommon()

ce5ab72133f808c94ce8d98ba90977430c97f403
jan-est <jan.tilles@est.tech>
2021-04-07 11:30:08 +0300

    This PR is fixing issue 4437. Webhook defaulting in v0.3.15 causing an error during KCP upgrade after upgrading cluster from v0.3.x to v0.3.15. This PR is changing every field under spec.rolloutStrategy mutable.

--- controlplane/kubeadm/api/v1alpha3/kubeadm_control_plane_webhook.go
+++ controlplane/kubeadm/api/v1alpha3/kubeadm_control_plane_webhook.go
@@ -139,7 +139,7 @@ func (in *KubeadmControlPlane) ValidateUpdate(old runtime.Object) error {
 		{spec, "version"},
 		{spec, "upgradeAfter"},
 		{spec, "nodeDrainTimeout"},
-		{spec, "rolloutStrategy"},
+		{spec, "rolloutStrategy", "*"},
 	}
 
 	allErrs := in.validateCommon()

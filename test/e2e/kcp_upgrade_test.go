75a97124baa00d849e3bb1ecfddb917bd79b31d4
Thomas Cordeu <info@tcordeu.com>
2021-05-27 12:35:42 -0300

    Divide KCPUpgrade E2E test suite

--- test/e2e/kcp_upgrade_test.go
+++ test/e2e/kcp_upgrade_test.go
@@ -22,18 +22,48 @@ import (
 	"context"
 
 	. "github.com/onsi/ginkgo"
+
+	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
 )
 
-var _ = Describe("When testing KCP upgrade", func() {
-
+var _ = Describe("When testing KCP upgrade in a single control plane cluster", func() {
 	KCPUpgradeSpec(context.TODO(), func() KCPUpgradeSpecInput {
 		return KCPUpgradeSpecInput{
-			E2EConfig:             e2eConfig,
-			ClusterctlConfigPath:  clusterctlConfigPath,
-			BootstrapClusterProxy: bootstrapClusterProxy,
-			ArtifactFolder:        artifactFolder,
-			SkipCleanup:           skipCleanup,
+			E2EConfig:                e2eConfig,
+			ClusterctlConfigPath:     clusterctlConfigPath,
+			BootstrapClusterProxy:    bootstrapClusterProxy,
+			ArtifactFolder:           artifactFolder,
+			SkipCleanup:              skipCleanup,
+			ControlPlaneMachineCount: 1,
+			Flavor:                   clusterctl.DefaultFlavor,
 		}
 	})
+})
 
+var _ = Describe("When testing KCP upgrade in a HA cluster", func() {
+	KCPUpgradeSpec(context.TODO(), func() KCPUpgradeSpecInput {
+		return KCPUpgradeSpecInput{
+			E2EConfig:                e2eConfig,
+			ClusterctlConfigPath:     clusterctlConfigPath,
+			BootstrapClusterProxy:    bootstrapClusterProxy,
+			ArtifactFolder:           artifactFolder,
+			SkipCleanup:              skipCleanup,
+			ControlPlaneMachineCount: 3,
+			Flavor:                   clusterctl.DefaultFlavor,
+		}
+	})
+})
+
+var _ = Describe("When testing KCP upgrade in a HA cluster using scale in rollout", func() {
+	KCPUpgradeSpec(context.TODO(), func() KCPUpgradeSpecInput {
+		return KCPUpgradeSpecInput{
+			E2EConfig:                e2eConfig,
+			ClusterctlConfigPath:     clusterctlConfigPath,
+			BootstrapClusterProxy:    bootstrapClusterProxy,
+			ArtifactFolder:           artifactFolder,
+			SkipCleanup:              skipCleanup,
+			ControlPlaneMachineCount: 3,
+			Flavor:                   "kcp-scale-in",
+		}
+	})
 })

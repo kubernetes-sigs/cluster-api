f95b3e6ab1b595c3fd8a582737d1e0631eaf9d04
Alexander Demichev <demichev.alexander@gmail.com>
2021-06-21 11:34:20 +0200

    Add generic reconciler

---
+++ exp/operator/controllers/genericprovider/genericprovider_interfaces.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package genericprovider
+
+import (
+	operatorv1 "sigs.k8s.io/cluster-api/exp/operator/api/v1alpha4"
+	"sigs.k8s.io/cluster-api/util/conditions"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type GenericProvider interface {
+	client.Object
+	conditions.Setter
+	GetSpec() operatorv1.ProviderSpec
+	SetSpec(in operatorv1.ProviderSpec)
+	GetStatus() operatorv1.ProviderStatus
+	SetStatus(in operatorv1.ProviderStatus)
+	GetObject() client.Object
+}
+
+type GenericProviderList interface {
+	client.ObjectList
+	GetObject() client.ObjectList
+	GetItems() []GenericProvider
+}
--- test/helpers/envtest.go
+++ test/helpers/envtest.go
@@ -48,6 +48,7 @@ import (
 	addonv1 "sigs.k8s.io/cluster-api/exp/addons/api/v1alpha4"
 	crs "sigs.k8s.io/cluster-api/exp/addons/api/v1alpha4"
 	expv1 "sigs.k8s.io/cluster-api/exp/api/v1alpha4"
+	operatorv1alpha4 "sigs.k8s.io/cluster-api/exp/operator/api/v1alpha4"
 	"sigs.k8s.io/cluster-api/util/kubeconfig"
 	utilyaml "sigs.k8s.io/cluster-api/util/yaml"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -81,6 +82,7 @@ func init() {
 	utilruntime.Must(addonv1.AddToScheme(scheme.Scheme))
 	utilruntime.Must(kcpv1.AddToScheme(scheme.Scheme))
 	utilruntime.Must(admissionv1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(operatorv1alpha4.AddToScheme(scheme.Scheme))
 
 	// Get the root of the current file to use in CRD paths.
 	_, filename, _, _ := goruntime.Caller(0) //nolint
@@ -93,6 +95,7 @@ func init() {
 			filepath.Join(root, "config", "crd", "bases"),
 			filepath.Join(root, "controlplane", "kubeadm", "config", "crd", "bases"),
 			filepath.Join(root, "bootstrap", "kubeadm", "config", "crd", "bases"),
+			filepath.Join(root, "exp", "operator", "config", "crd", "bases"),
 		},
 		CRDs: []client.Object{
 			external.TestGenericBootstrapCRD.DeepCopy(),

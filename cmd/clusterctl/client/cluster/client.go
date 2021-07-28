kubernetes-sigs/clife_cluster-api

block clusterctl move and init on Kubernetes v1.22
Signed-off-by: Stefan Büringer buringerst@vmware.com

cmd/clusterctl/client/cluster/client.go
@@ -33,6 +33,9 @@ import (
 
 const (
 	minimumKubernetesVersion = "v1.16.0"
+	// firstUnsupportedKubernetesVersion is the first version we don't support as management cluster.
+	// We cannot support v1.22.0 as it's not compatible with our controller-runtime version and WebhookConfigurations v1beta1.
+	firstUnsupportedKubernetesVersion = "v1.22.0"
 )
 
 var (
@@ -227,6 +230,9 @@ type Proxy interface {
 
 	// ValidateKubernetesVersion returns an error if management cluster version less than minimumKubernetesVersion
 	ValidateKubernetesVersion() error
+
+	// ValidateKubernetesMaxVersion returns an error if management cluster version is greater than or equal to firstUnsupportedKubernetesVersion
+	ValidateKubernetesMaxVersion() error
 
 	// NewClient returns a new controller runtime Client object for working on the management cluster
 	NewClient() (client.Client, error)

cmd/clusterctl/client/cluster/proxy.go
@@ -100,6 +100,31 @@ func (k *proxy) ValidateKubernetesVersion() error {
 	return nil
 }
 
+func (k *proxy) ValidateKubernetesMaxVersion() error {
+	config, err := k.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	client := discovery.NewDiscoveryClientForConfigOrDie(config)
+	serverVersion, err := client.ServerVersion()
+	if err != nil {
+		return errors.Wrap(err, "failed to retrieve server version")
+	}
+
+	compver, err := utilversion.MustParseGeneric(serverVersion.String()).Compare(firstUnsupportedKubernetesVersion)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse and compare server version")
+	}
+
+	// We want to return an error if the serverVersion is greater or equal than firstUnsupportedKubernetesVersion
+	if compver >= 0 {
+		return errors.Errorf("unsupported management cluster server version: %s - versions greater or equal than %s are not supported", serverVersion.String(), firstUnsupportedKubernetesVersion)
+	}
+
+	return nil
+}
+
 // GetConfig returns the config for a kubernetes client.
 func (k *proxy) GetConfig() (*rest.Config, error) {
 	config, err := k.configLoadingRules.Load()

kubernetes-sigs/cluster-api

cmd/clusterctl/client/cluster/client.go
@@ -33,6 +33,9 @@ import (
 
 const (
 	minimumKubernetesVersion = "v1.16.0"
+	// managementClusterVersionCeiling is the first version we don't support as management cluster.
+	// We cannot support v1.22.0 as it's not compatible with our controller-runtime version and WebhookConfigurations v1beta1.
+	managementClusterVersionCeiling = "v1.22.0"
 )
 
 var (
@@ -227,6 +230,9 @@ type Proxy interface {
 
 	// ValidateKubernetesVersion returns an error if management cluster version less than minimumKubernetesVersion
 	ValidateKubernetesVersion() error
+
+	// ValidateKubernetesMaxVersion returns an error if management cluster version is greater than or equal to managementClusterVersionCeiling
+	ValidateKubernetesMaxVersion() error
 
 	// NewClient returns a new controller runtime Client object for working on the management cluster
 	NewClient() (client.Client, error)

cmd/clusterctl/client/cluster/proxy.go
@@ -100,6 +100,31 @@ func (k *proxy) ValidateKubernetesVersion() error {
 	return nil
 }
 
+func (k *proxy) ValidateKubernetesMaxVersion() error {
+	config, err := k.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	client := discovery.NewDiscoveryClientForConfigOrDie(config)
+	serverVersion, err := client.ServerVersion()
+	if err != nil {
+		return errors.Wrap(err, "failed to retrieve server version")
+	}
+
+	compver, err := utilversion.MustParseGeneric(serverVersion.String()).Compare(managementClusterVersionCeiling)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse and compare server version")
+	}
+
+	// We want to return an error if the serverVersion is greater or equal than managementClusterVersionCeiling
+	if compver >= 0 {
+		return errors.Errorf("unsupported management cluster server version: %s - versions greater or equal than %s are not supported", serverVersion.String(), managementClusterVersionCeiling)
+	}
+
+	return nil
+}
+
 // GetConfig returns the config for a kubernetes client.
 func (k *proxy) GetConfig() (*rest.Config, error) {
 	config, err := k.configLoadingRules.Load()
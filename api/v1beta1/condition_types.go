9377ba2a19ea69f716f88689f6a550698d4da4b2
fabriziopandini <fpandini@vmware.com>
2021-09-23 16:43:39 +0200

    make condition.lastTransitionTime required

--- api/v1beta1/condition_types.go
+++ api/v1beta1/condition_types.go
@@ -70,7 +70,7 @@ type Condition struct {
 	// Last time the condition transitioned from one status to another.
 	// This should be when the underlying condition changed. If that is not known, then using the time when
 	// the API field changed is acceptable.
-	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+	LastTransitionTime metav1.Time `json:"lastTransitionTime"`
 
 	// The reason for the condition's last transition in CamelCase.
 	// The specific API may choose whether or not this field is considered a guaranteed API.

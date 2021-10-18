1ae46c3e8ed962f20cf451ca5e137fba5471da64
Sagar Muchhal <muchhals@vmware.com>
2021-10-18 15:58:22 -0700

    Deprecates drain code copied from k8s
    - This patch removes the usage of local copy of the drain code from the repo and
    instead uses the vendored code from the k8s.io/pkg/drain package.
    - This also adds a deprecation notice to the code under the
      third_party/kubernetes-drain directory.
    
    Signed-off-by: Sagar Muchhal <muchhals@vmware.com>

--- controllers/machine_controller.go
+++ controllers/machine_controller.go
@@ -32,12 +32,12 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
+	kubedrain "k8s.io/kubectl/pkg/drain"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/api/v1beta1/index"
 	"sigs.k8s.io/cluster-api/controllers/external"
 	"sigs.k8s.io/cluster-api/controllers/noderefutil"
 	"sigs.k8s.io/cluster-api/controllers/remote"
-	kubedrain "sigs.k8s.io/cluster-api/third_party/kubernetes-drain"
 	"sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/annotations"
 	"sigs.k8s.io/cluster-api/util/collections"
@@ -512,9 +512,10 @@ func (r *MachineReconciler) drainNode(ctx context.Context, cluster *clusterv1.Cl
 
 	drainer := &kubedrain.Helper{
 		Client:              kubeClient,
+		Ctx:                 ctx,
 		Force:               true,
 		IgnoreAllDaemonSets: true,
-		DeleteLocalData:     true,
+		DeleteEmptyDirData:  true,
 		GracePeriodSeconds:  -1,
 		// If a pod is not evicted in 20 seconds, retry the eviction next time the
 		// machine gets reconciled again (to allow other machines to be reconciled).
@@ -529,7 +530,6 @@ func (r *MachineReconciler) drainNode(ctx context.Context, cluster *clusterv1.Cl
 		},
 		Out:    writer{klog.Info},
 		ErrOut: writer{klog.Error},
-		DryRun: false,
 	}
 
 	if noderefutil.IsNodeUnreachable(node) {
@@ -537,13 +537,13 @@ func (r *MachineReconciler) drainNode(ctx context.Context, cluster *clusterv1.Cl
 		drainer.SkipWaitForDeleteTimeoutSeconds = 60 * 5 // 5 minutes
 	}
 
-	if err := kubedrain.RunCordonOrUncordon(ctx, drainer, node, true); err != nil {
+	if err := kubedrain.RunCordonOrUncordon(drainer, node, true); err != nil {
 		// Machine will be re-reconciled after a cordon failure.
 		log.Error(err, "Cordon failed")
 		return ctrl.Result{}, errors.Errorf("unable to cordon node %s: %v", node.Name, err)
 	}
 
-	if err := kubedrain.RunNodeDrain(ctx, drainer, node.Name); err != nil {
+	if err := kubedrain.RunNodeDrain(drainer, node.Name); err != nil {
 		// Machine will be re-reconciled after a drain failure.
 		log.Error(err, "Drain failed, retry in 20s")
 		return ctrl.Result{RequeueAfter: 20 * time.Second}, nil
--- third_party/kubernetes-drain/README.md
+++ third_party/kubernetes-drain/README.md
@@ -1,2 +1,4 @@
+NOTE: The types and methods in this directory have been deprecated and will be removed in a future release.
+
 The code in this directory has been copied from:
 github.com/kubernetes/kubectl/pkg/drain@a17d91f9f5b34c73bed0bfc75b70bd762b725231

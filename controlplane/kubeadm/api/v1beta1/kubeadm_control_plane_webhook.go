ed835d6d7d72dc5f39da42a977e6d69cc059323c
Stefan Bueringer <buringerst@vmware.com>
2021-11-15 15:19:18 +0100

    Make KCP nodeDrainTimeout mutable
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook.go
+++ controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook.go
@@ -140,10 +140,10 @@ func (in *KubeadmControlPlane) ValidateUpdate(old runtime.Object) error {
 		{spec, "machineTemplate", "metadata", "*"},
 		{spec, "machineTemplate", "infrastructureRef", "apiVersion"},
 		{spec, "machineTemplate", "infrastructureRef", "name"},
+		{spec, "machineTemplate", "nodeDrainTimeout"},
 		{spec, "replicas"},
 		{spec, "version"},
 		{spec, "rolloutAfter"},
-		{spec, "nodeDrainTimeout"},
 		{spec, "rolloutStrategy", "*"},
 	}
 

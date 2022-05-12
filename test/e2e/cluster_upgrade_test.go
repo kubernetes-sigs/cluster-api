bbfb5ded6edc3f15780e4dbe41539ebeb14d3164
Christian Schlotter <christi.schlotter@gmail.com>
2022-05-12 18:19:59 +0200

    Use Kubernetes 1.24 in quickstart and CAPD, bump to kind v0.14
    * Use Kubernetes 1.24 in quickstart and CAPD
    * Bumps kind to v0.14 for v1.24 support as kind management cluster
    * Migrates CI upgrade tests to use cluster class and version aware patches for cgroupDriver
    * Implement workaround for DockerMachinePools which are not supported by ClusterClass to use correct cgroupDriver on upgrade tests.
    
    Co-authored-by: killianmuldoon <kmuldoon@vmware.com>
    Co-authored-by: sbueringer <buringerst@vmware.com>

--- test/e2e/cluster_upgrade_test.go
+++ test/e2e/cluster_upgrade_test.go
@@ -20,20 +20,44 @@ limitations under the License.
 package e2e
 
 import (
+	"github.com/blang/semver"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"k8s.io/utils/pointer"
-
-	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
 )
 
-var _ = Describe("When upgrading a workload cluster and testing K8S conformance [Conformance] [K8s-Upgrade]", func() {
+var _ = Describe("When upgrading a workload cluster using ClusterClass and testing K8S conformance [Conformance] [K8s-Upgrade]", func() {
 	ClusterUpgradeConformanceSpec(ctx, func() ClusterUpgradeConformanceSpecInput {
+		// "upgrades" is the same as the "topology" flavor but with an additional MachinePool.
+		flavor := pointer.String("upgrades")
+		// For KubernetesVersionUpgradeFrom < v1.24 we have to use upgrades-cgroupfs flavor.
+		// This is because kind and CAPD only support:
+		// * cgroupDriver cgroupfs for Kubernetes < v1.24
+		// * cgroupDriver systemd for Kubernetes >= v1.24.
+		// Notes:
+		// * We always use a ClusterClass-based cluster-template for the upgrade test
+		// * The ClusterClass will automatically adjust the cgroupDriver for KCP and MDs.
+		// * We have to handle the MachinePool ourselves
+		// * The upgrades-cgroupfs flavor uses an MP which is pinned to cgroupfs
+		// * During the upgrade UpgradeMachinePoolAndWait automatically drops the cgroupfs pinning
+		//   when the target version is >= v1.24.
+		// We can remove this as soon as we don't test upgrades from Kubernetes < v1.24 anymore with CAPD
+		// or MachinePools are supported in ClusterClass.
+		version, err := semver.ParseTolerant(e2eConfig.Variables[KubernetesVersionUpgradeFrom])
+		Expect(err).ToNot(HaveOccurred(), "Invalid argument, KUBERNETES_VERSION_UPGRADE_FROM is not a valid version")
+		if version.LT(semver.MustParse("1.24.0")) {
+			// "upgrades-cgroupfs" is the same as the "topology" flavor but with an additional MachinePool
+			// with pinned cgroupDriver to cgroupfs.
+			flavor = pointer.String("upgrades-cgroupfs")
+		}
+
 		return ClusterUpgradeConformanceSpecInput{
 			E2EConfig:             e2eConfig,
 			ClusterctlConfigPath:  clusterctlConfigPath,
 			BootstrapClusterProxy: bootstrapClusterProxy,
 			ArtifactFolder:        artifactFolder,
 			SkipCleanup:           skipCleanup,
+			Flavor:                flavor,
 		}
 	})
 })
@@ -49,30 +73,14 @@ var _ = Describe("When upgrading a workload cluster using ClusterClass", func() 
 			Flavor:                pointer.String("topology"),
 			// This test is run in CI in parallel with other tests. To keep the test duration reasonable
 			// the conformance tests are skipped.
-			SkipConformanceTests: true,
-		}
-	})
-})
-
-var _ = Describe("When upgrading a workload cluster with a single control plane machine", func() {
-	ClusterUpgradeConformanceSpec(ctx, func() ClusterUpgradeConformanceSpecInput {
-		return ClusterUpgradeConformanceSpecInput{
-			E2EConfig:             e2eConfig,
-			ClusterctlConfigPath:  clusterctlConfigPath,
-			BootstrapClusterProxy: bootstrapClusterProxy,
-			ArtifactFolder:        artifactFolder,
-			SkipCleanup:           skipCleanup,
-			// This test is run in CI in parallel with other tests. To keep the test duration reasonable
-			// the conformance tests are skipped.
-			SkipConformanceTests:     true,
 			ControlPlaneMachineCount: pointer.Int64(1),
-			WorkerMachineCount:       pointer.Int64(1),
-			Flavor:                   pointer.String(clusterctl.DefaultFlavor),
+			WorkerMachineCount:       pointer.Int64(2),
+			SkipConformanceTests:     true,
 		}
 	})
 })
 
-var _ = Describe("When upgrading a workload cluster with a HA control plane", func() {
+var _ = Describe("When upgrading a workload cluster using ClusterClass with a HA control plane", func() {
 	ClusterUpgradeConformanceSpec(ctx, func() ClusterUpgradeConformanceSpecInput {
 		return ClusterUpgradeConformanceSpecInput{
 			E2EConfig:             e2eConfig,
@@ -85,12 +93,12 @@ var _ = Describe("When upgrading a workload cluster with a HA control plane", fu
 			SkipConformanceTests:     true,
 			ControlPlaneMachineCount: pointer.Int64(3),
 			WorkerMachineCount:       pointer.Int64(1),
-			Flavor:                   pointer.String(clusterctl.DefaultFlavor),
+			Flavor:                   pointer.String("topology"),
 		}
 	})
 })
 
-var _ = Describe("When upgrading a workload cluster with a HA control plane using scale-in rollout", func() {
+var _ = Describe("When upgrading a workload cluster using ClusterClass with a HA control plane using scale-in rollout", func() {
 	ClusterUpgradeConformanceSpec(ctx, func() ClusterUpgradeConformanceSpecInput {
 		return ClusterUpgradeConformanceSpecInput{
 			E2EConfig:             e2eConfig,

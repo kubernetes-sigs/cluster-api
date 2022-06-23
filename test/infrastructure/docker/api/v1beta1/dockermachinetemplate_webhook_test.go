0571cc51ca53bb568dd7b62d3c66b62498550c16
Christian Schlotter <christi.schlotter@gmail.com>
2022-06-23 10:56:49 +0200

    Improve dry run for topology changes to dry run server side apply
    * Adds helper function for CustomValidators to skip immutability checks
    * CAPD: skip immutability checks for topology dry run
    * book: add provider documentation
    
    Co-authored-by: fabriziopandini <fpandini@vmware.com>

--- test/infrastructure/docker/api/v1beta1/dockermachinetemplate_webhook_test.go
+++ test/infrastructure/docker/api/v1beta1/dockermachinetemplate_webhook_test.go
@@ -17,9 +17,15 @@ limitations under the License.
 package v1beta1
 
 import (
+	"context"
 	"testing"
 
+	admissionv1 "k8s.io/api/admission/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 func TestDockerMachineTemplateInvalid(t *testing.T) {
@@ -32,29 +38,60 @@ func TestDockerMachineTemplateInvalid(t *testing.T) {
 
 	newTemplate := oldTemplate.DeepCopy()
 	newTemplate.Spec.Template.Spec.ExtraMounts = append(newTemplate.Spec.Template.Spec.ExtraMounts, []Mount{{ContainerPath: "/var/run/docker.sock", HostPath: "/var/run/docker.sock"}}...)
+	newTemplateSkipImmutabilityAnnotationSet := newTemplate.DeepCopy()
+	newTemplateSkipImmutabilityAnnotationSet.SetAnnotations(map[string]string{clusterv1.TopologyDryRunAnnotation: ""})
 
 	tests := []struct {
 		name        string
 		newTemplate *DockerMachineTemplate
 		oldTemplate *DockerMachineTemplate
+		req         *admission.Request
 		wantError   bool
 	}{
 		{
 			name:        "return no error if no modification",
 			newTemplate: newTemplate,
 			oldTemplate: newTemplate,
+			req:         &admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(false)}},
 			wantError:   false,
 		},
 		{
 			name:        "don't allow modification",
 			newTemplate: newTemplate,
 			oldTemplate: &oldTemplate,
+			req:         &admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(false)}},
 			wantError:   true,
+		},
+		{
+			name:        "don't allow modification, skip immutability annotation set",
+			newTemplate: newTemplateSkipImmutabilityAnnotationSet,
+			oldTemplate: &oldTemplate,
+			req:         &admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(false)}},
+			wantError:   true,
+		},
+		{
+			name:        "don't allow modification, dry run, no skip immutability annotation set",
+			newTemplate: newTemplate,
+			oldTemplate: &oldTemplate,
+			req:         &admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(true)}},
+			wantError:   true,
+		},
+		{
+			name:        "skip immutability check",
+			newTemplate: newTemplateSkipImmutabilityAnnotationSet,
+			oldTemplate: &oldTemplate,
+			req:         &admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(true)}},
+			wantError:   false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := tt.newTemplate.ValidateUpdate(tt.oldTemplate)
+			wh := &DockerMachineTemplateWebhook{}
+			ctx := context.Background()
+			if tt.req != nil {
+				ctx = admission.NewContextWithRequest(ctx, *tt.req)
+			}
+			err := wh.ValidateUpdate(ctx, tt.oldTemplate, tt.newTemplate)
 			if (err != nil) != tt.wantError {
 				t.Errorf("unexpected result - wanted %+v, got %+v", tt.wantError, err)
 			}
---
+++ util/topology/topology_test.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package topology implements topology utility functions.
+package topology
+
+import (
+	"testing"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+)
+
+func TestShouldSkipImmutabilityChecks(t *testing.T) {
+	tests := []struct {
+		name string
+		req  admission.Request
+		obj  metav1.Object
+		want bool
+	}{
+		{
+			name: "false - dryRun pointer is nil",
+			req:  admission.Request{},
+			obj:  nil,
+			want: false,
+		},
+		{
+			name: "false - dryRun pointer is false",
+			req:  admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(false)}},
+			obj:  nil,
+			want: false,
+		},
+		{
+			name: "false - nil obj",
+			req:  admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(true)}},
+			obj:  nil,
+			want: false,
+		},
+		{
+			name: "false - no annotations",
+			req:  admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(true)}},
+			obj:  &unstructured.Unstructured{},
+			want: false,
+		},
+		{
+			name: "false - annotation not set",
+			req:  admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(true)}},
+			obj: &unstructured.Unstructured{Object: map[string]interface{}{
+				"metadata": map[string]interface{}{
+					"annotations": map[string]interface{}{},
+				},
+			}},
+			want: false,
+		},
+		{
+			name: "true",
+			req:  admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{DryRun: pointer.Bool(true)}},
+			obj: &unstructured.Unstructured{Object: map[string]interface{}{
+				"metadata": map[string]interface{}{
+					"annotations": map[string]interface{}{
+						clusterv1.TopologyDryRunAnnotation: "",
+					},
+				},
+			}},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ShouldSkipImmutabilityChecks(tt.req, tt.obj); got != tt.want {
+				t.Errorf("ShouldSkipImmutabilityChecks() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

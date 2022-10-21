993559d1a46186b4ead30a30fce63e81e2b28af1
Stefan Bueringer <sbueringer@gmail.com>
2022-10-21 13:32:00 +0200

    KCP: make rolloutBefore.certificatesExpiryDays mutable
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook.go
+++ controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook.go
@@ -168,6 +168,7 @@ func (in *KubeadmControlPlane) ValidateUpdate(old runtime.Object) error {
 		{spec, "replicas"},
 		{spec, "version"},
 		{spec, "rolloutAfter"},
+		{spec, "rolloutBefore", "*"},
 		{spec, "rolloutStrategy", "*"},
 	}
 

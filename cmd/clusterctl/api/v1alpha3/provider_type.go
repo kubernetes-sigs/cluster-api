da6769b5460c5f6baa4f7cd98eea678241b5d286
killianmuldoon <kmuldoon@vmware.com>
2023-03-06 18:08:20 +0000

    Fix v1alpha4 references in clusterctl comments

--- cmd/clusterctl/api/v1alpha3/provider_type.go
+++ cmd/clusterctl/api/v1alpha3/provider_type.go
@@ -50,7 +50,7 @@ type Provider struct {
 	// WatchedNamespace indicates the namespace where the provider controller is watching.
 	// If empty the provider controller is watching for objects in all namespaces.
 	//
-	// Deprecated: in clusterctl v1alpha4 all the providers watch all the namespaces; this field will be removed in a future version of this API
+	// Deprecated: providers complying with the Cluster API v1alpha4 contract or above must watch all namespaces; this field will be removed in a future version of this API
 	// +optional
 	WatchedNamespace string `json:"watchedNamespace,omitempty"`
 }

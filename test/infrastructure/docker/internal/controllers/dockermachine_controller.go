cbde5a2f365a8ff783f3ce6613d5fcd70cb0f12a
Stefan Bueringer <sbueringer@gmail.com>
2023-06-05 19:03:00 +0200

    CAPD: Implement watch filter
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- test/infrastructure/docker/internal/controllers/dockermachine_controller.go
+++ test/infrastructure/docker/internal/controllers/dockermachine_controller.go
@@ -53,6 +53,9 @@ type DockerMachineReconciler struct {
 	client.Client
 	ContainerRuntime container.Runtime
 	Tracker          *remote.ClusterCacheTracker
+
+	// WatchFilterValue is the label value used to filter events prior to reconciliation.
+	WatchFilterValue string
 }
 
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockermachines,verbs=get;list;watch;create;update;patch;delete
@@ -427,7 +430,7 @@ func (r *DockerMachineReconciler) SetupWithManager(ctx context.Context, mgr ctrl
 	err = ctrl.NewControllerManagedBy(mgr).
 		For(&infrav1.DockerMachine{}).
 		WithOptions(options).
-		WithEventFilter(predicates.ResourceNotPaused(ctrl.LoggerFrom(ctx))).
+		WithEventFilter(predicates.ResourceNotPausedAndHasFilterLabel(ctrl.LoggerFrom(ctx), r.WatchFilterValue)).
 		Watches(
 			&clusterv1.Machine{},
 			handler.EnqueueRequestsFromMapFunc(util.MachineToInfrastructureMapFunc(infrav1.GroupVersion.WithKind("DockerMachine"))),

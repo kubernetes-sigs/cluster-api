ee574d90f1518ed7030ab3e66d8e614fe38ab188
Furkat Gofurov <furkat.gofurov@suse.com>
2023-08-07 18:33:05 +0300

    Fix lint reported issue
    Signed-off-by: Furkat Gofurov <furkat.gofurov@suse.com>

--- controllers/noderefutil/util.go
+++ controllers/noderefutil/util.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package noderefutil implements noderef utilities.
 package noderefutil
 
 import (

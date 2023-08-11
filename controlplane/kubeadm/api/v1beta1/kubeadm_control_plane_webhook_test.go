c1f3d78a80d26d46f69242487c5edf0e2adbc35e
Max Fedotov <m.a.fedotov@gmail.com>
2023-08-11 18:11:33 +0300

    Allow to drop useExperimentalRetryJoin field from KubeadmControlPlane.kubeadmConfigSpec

--- controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook_test.go
+++ controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook_test.go
@@ -671,6 +671,11 @@ func TestKubeadmControlPlaneValidateUpdate(t *testing.T) {
 		{"/var/lib/testdir", "/var/lib/etcd/data"},
 	}
 
+	beforeUseExperimentalRetryJoin := before.DeepCopy()
+	beforeUseExperimentalRetryJoin.Spec.KubeadmConfigSpec.UseExperimentalRetryJoin = true //nolint:staticcheck
+	updateUseExperimentalRetryJoin := before.DeepCopy()
+	updateUseExperimentalRetryJoin.Spec.KubeadmConfigSpec.UseExperimentalRetryJoin = false //nolint:staticcheck
+
 	tests := []struct {
 		name                  string
 		enableIgnitionFeature bool
@@ -1015,6 +1020,12 @@ func TestKubeadmControlPlaneValidateUpdate(t *testing.T) {
 			expectErr:             false,
 			before:                before,
 			kcp:                   switchFromCloudInitToIgnition,
+		},
+		{
+			name:      "should allow changes to useExperimentalRetryJoin",
+			expectErr: false,
+			before:    beforeUseExperimentalRetryJoin,
+			kcp:       updateUseExperimentalRetryJoin,
 		},
 	}
 

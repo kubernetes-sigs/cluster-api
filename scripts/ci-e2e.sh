d9863b5956b3d6fb129734bbdf7477f1a5cb22d1
Stefan Bueringer <buringerst@vmware.com>
2023-08-14 07:39:37 +0200

    test/e2e: increase concurrency
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -36,7 +36,7 @@ export PATH="${REPO_ROOT}/hack/tools/bin:${PATH}"
 capi:buildDockerImages
 
 # Configure e2e tests
-export GINKGO_NODES=3
+export GINKGO_NODES=8
 export GINKGO_NOCOLOR=true
 export GINKGO_ARGS="${GINKGO_ARGS:-""}"
 export E2E_CONF_FILE="${REPO_ROOT}/test/e2e/config/docker.yaml"
@@ -93,9 +93,9 @@ cleanup() {
 
   for PID in $(ps -eo pid=); do
     echo "> PID=$PID"
-    echo ">> /proc/${PID}/status" 
+    echo ">> /proc/${PID}/status"
     cat "/proc/${PID}/status" || true
-    echo ">> /proc/${PID}/stack" 
+    echo ">> /proc/${PID}/stack"
     cat "/proc/${PID}/stack" || true
   done >> "${ARTIFACTS_LOCAL}/processes-proc-information.txt"
 

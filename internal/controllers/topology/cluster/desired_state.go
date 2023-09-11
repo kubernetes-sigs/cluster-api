e90bc0bcabeb372082563ed9db4602eb9d6b3792
Stefan Bueringer <buringerst@vmware.com>
2023-09-11 12:55:08 +0200

    Add ownerRefs to BootstrapConfig/InfraMachinePool in classy Clusters
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- internal/controllers/topology/cluster/desired_state.go
+++ internal/controllers/topology/cluster/desired_state.go
@@ -617,7 +617,7 @@ func computeMachineDeployment(ctx context.Context, s *scope.Scope, machineDeploy
 		currentObjectRef:      currentBootstrapTemplateRef,
 		// Note: we are adding an ownerRef to Cluster so the template will be automatically garbage collected
 		// in case of errors in between creating this template and creating/updating the MachineDeployment object
-		// with the reference to the ControlPlane object using this template.
+		// with the reference to this template.
 		ownerRef: ownerReferenceTo(s.Current.Cluster),
 	})
 
@@ -642,7 +642,7 @@ func computeMachineDeployment(ctx context.Context, s *scope.Scope, machineDeploy
 		currentObjectRef:      currentInfraMachineTemplateRef,
 		// Note: we are adding an ownerRef to Cluster so the template will be automatically garbage collected
 		// in case of errors in between creating this template and creating/updating the MachineDeployment object
-		// with the reference to the ControlPlane object using this template.
+		// with the reference to this template.
 		ownerRef: ownerReferenceTo(s.Current.Cluster),
 	})
 
@@ -953,6 +953,10 @@ func computeMachinePool(_ context.Context, s *scope.Scope, machinePoolTopology c
 		cluster:               s.Current.Cluster,
 		namePrefix:            bootstrapConfigNamePrefix(s.Current.Cluster.Name, machinePoolTopology.Name),
 		currentObjectRef:      currentBootstrapConfigRef,
+		// Note: we are adding an ownerRef to Cluster so the template will be automatically garbage collected
+		// in case of errors in between creating this template and creating/updating the MachinePool object
+		// with the reference to this template.
+		ownerRef: ownerReferenceTo(s.Current.Cluster),
 	})
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to compute bootstrap object for topology %q", machinePoolTopology.Name)
@@ -977,6 +981,10 @@ func computeMachinePool(_ context.Context, s *scope.Scope, machinePoolTopology c
 		cluster:               s.Current.Cluster,
 		namePrefix:            infrastructureMachinePoolNamePrefix(s.Current.Cluster.Name, machinePoolTopology.Name),
 		currentObjectRef:      currentInfraMachinePoolRef,
+		// Note: we are adding an ownerRef to Cluster so the template will be automatically garbage collected
+		// in case of errors in between creating this template and creating/updating the MachinePool object
+		// with the reference to this template.
+		ownerRef: ownerReferenceTo(s.Current.Cluster),
 	})
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to compute infrastructure object for topology %q", machinePoolTopology.Name)

206e291a6aca8aa0ec8acfe5cd0bff3ac4b451f5
Christian Schlotter <christi.schlotter@gmail.com>
2023-11-27 18:14:19 +0100

    clusterresourceset: requeue after 1 minute if ErrClusterLocked got hit

--- exp/addons/internal/controllers/clusterresourceset_controller.go
+++ exp/addons/internal/controllers/clusterresourceset_controller.go
@@ -151,7 +151,7 @@ func (r *ClusterResourceSetReconciler) Reconcile(ctx context.Context, req ctrl.R
 			// the current cluster because of concurrent access.
 			if errors.Is(err, remote.ErrClusterLocked) {
 				log.V(5).Info("Requeuing because another worker has the lock on the ClusterCacheTracker")
-				return ctrl.Result{Requeue: true}, nil
+				return ctrl.Result{RequeueAfter: time.Minute}, nil
 			}
 			return ctrl.Result{}, err
 		}

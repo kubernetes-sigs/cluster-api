d23bb93214e25b8e3e7d03339449a256f7e46ac6
Christian Schlotter <schlotterc@vmware.com>
2023-12-13 12:06:14 +0100

    v1.29: Prepare quickstart, capd and tests for the new release including kind bump

--- test/infrastructure/kind/mapper.go
+++ test/infrastructure/kind/mapper.go
@@ -82,6 +82,11 @@ var preBuiltMappings = []Mapping{
 
 	// Pre-built images for Kind v1.20.
 	{
+		KubernetesVersion: semver.MustParse("1.29.0"),
+		Mode:              Mode0_20,
+		Image:             "kindest/node:v1.29.0@sha256:eaa1450915475849a73a9227b8f201df25e55e268e5d619312131292e324d570",
+	},
+	{
 		KubernetesVersion: semver.MustParse("1.28.0"),
 		Mode:              Mode0_20,
 		Image:             "kindest/node:v1.28.0@sha256:b7a4cad12c197af3ba43202d3efe03246b3f0793f162afb40a33c923952d5b31",

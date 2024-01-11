d710bfbff9744fc72c165bf064415394614a121d
Christian Schlotter <schlotterc@vmware.com>
2024-01-11 13:57:54 +0100

    test/framework add WatchDaemonSetLogsByLabelSelector method

--- test/framework/daemonset_helpers.go
+++ test/framework/daemonset_helpers.go
@@ -24,6 +24,8 @@ import (
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	toolscache "sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"sigs.k8s.io/cluster-api/internal/util/kubeadm"
@@ -59,4 +61,40 @@ func WaitForKubeProxyUpgrade(ctx context.Context, input WaitForKubeProxyUpgradeI
 		}
 		return false, nil
 	}, intervals...).Should(BeTrue())
+}
+
+// WatchDaemonSetLogsByLabelSelectorInput is the input for WatchDaemonSetLogsByLabelSelector.
+type WatchDaemonSetLogsByLabelSelectorInput struct {
+	GetLister GetLister
+	Cache     toolscache.Cache
+	ClientSet *kubernetes.Clientset
+	Labels    map[string]string
+	LogPath   string
+}
+
+// WatchDaemonSetLogsByLabelSelector streams logs for all containers for all pods belonging to a daemonset on the basis of label. Each container's logs are streamed
+// in a separate goroutine so they can all be streamed concurrently. This only causes a test failure if there are errors
+// retrieving the daemonset, its pods, or setting up a log file. If there is an error with the log streaming itself,
+// that does not cause the test to fail.
+func WatchDaemonSetLogsByLabelSelector(ctx context.Context, input WatchDaemonSetLogsByLabelSelectorInput) {
+	Expect(ctx).NotTo(BeNil(), "ctx is required for WatchDaemonSetLogsByLabelSelector")
+	Expect(input.Cache).NotTo(BeNil(), "input.Cache is required for WatchDaemonSetLogsByLabelSelector")
+	Expect(input.ClientSet).NotTo(BeNil(), "input.ClientSet is required for WatchDaemonSetLogsByLabelSelector")
+	Expect(input.Labels).NotTo(BeNil(), "input.Selector is required for WatchDaemonSetLogsByLabelSelector")
+
+	daemonSetList := &appsv1.DaemonSetList{}
+	Eventually(func() error {
+		return input.GetLister.List(ctx, daemonSetList, client.MatchingLabels(input.Labels))
+	}, retryableOperationTimeout, retryableOperationInterval).Should(Succeed(), "Failed to get DaemonSets for labels")
+
+	for _, daemonSet := range daemonSetList.Items {
+		watchPodLogs(ctx, watchPodLogsInput{
+			Cache:                input.Cache,
+			ClientSet:            input.ClientSet,
+			Namespace:            daemonSet.Namespace,
+			ManagingResourceName: daemonSet.Name,
+			LabelSelector:        daemonSet.Spec.Selector,
+			LogPath:              input.LogPath,
+		})
+	}
 }

37e64cc9271ff245bda33c7ee5c67cbe93c378fd
Stefan Bueringer <buringerst@vmware.com>
2024-02-05 13:51:31 +0100

    Use manager in test extension
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- test/extension/main.go
+++ test/extension/main.go
@@ -21,22 +21,28 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
+	goruntime "runtime"
 	"time"
 
 	"github.com/spf13/pflag"
+	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/logs"
 	logsv1 "k8s.io/component-base/logs/api/v1"
 	_ "k8s.io/component-base/logs/json/register"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
 	"sigs.k8s.io/cluster-api/controllers/remote"
 	controlplanev1 "sigs.k8s.io/cluster-api/controlplane/kubeadm/api/v1beta1"
@@ -46,6 +52,7 @@ import (
 	"sigs.k8s.io/cluster-api/test/extension/handlers/lifecycle"
 	"sigs.k8s.io/cluster-api/test/extension/handlers/topologymutation"
 	infrav1 "sigs.k8s.io/cluster-api/test/infrastructure/docker/api/v1beta1"
+	"sigs.k8s.io/cluster-api/util/flags"
 	"sigs.k8s.io/cluster-api/version"
 )
 
@@ -55,25 +62,44 @@ var (
 
 	// scheme is a Kubernetes runtime scheme containing all the information about API types used by the test extension.
 	// NOTE: it is not mandatory to use scheme in custom RuntimeExtension, but working with typed API objects makes code
-	// easier to read and less error prone than using unstructured or working with raw json/yaml.
-	scheme         = runtime.NewScheme()
+	// easier to read and less error-prone than using unstructured or working with raw json/yaml.
+	scheme = runtime.NewScheme()
+	// Creates a logger to be used during the main func using controller runtime utilities
+	// NOTE: it is not mandatory to use controller runtime utilities in custom RuntimeExtension, but it is recommended
+	// because it makes log from those components similar to log from controllers.
+	setupLog       = ctrl.Log.WithName("setup")
 	controllerName = "cluster-api-test-extension-manager"
 
-	// Flags.
-	profilerAddress string
-	webhookPort     int
-	webhookCertDir  string
-	logOptions      = logs.NewOptions()
+	// flags.
+	enableLeaderElection        bool
+	leaderElectionLeaseDuration time.Duration
+	leaderElectionRenewDeadline time.Duration
+	leaderElectionRetryPeriod   time.Duration
+	profilerAddress             string
+	enableContentionProfiling   bool
+	syncPeriod                  time.Duration
+	restConfigQPS               float32
+	restConfigBurst             int
+	webhookPort                 int
+	webhookCertDir              string
+	healthAddr                  string
+	tlsOptions                  = flags.TLSOptions{}
+	diagnosticsOptions          = flags.DiagnosticsOptions{}
+	logOptions                  = logs.NewOptions()
 )
 
 func init() {
-	// Adds to the catalog all the RuntimeHooks defined in cluster API.
-	_ = runtimehooksv1.AddToCatalog(catalog)
-
 	// Adds to the scheme all the API types we used by the test extension.
-	_ = infrav1.AddToScheme(scheme)
-	_ = controlplanev1.AddToScheme(scheme)
+	_ = clientgoscheme.AddToScheme(scheme)
+	_ = apiextensionsv1.AddToScheme(scheme)
+
+	_ = clusterv1.AddToScheme(scheme)
 	_ = bootstrapv1.AddToScheme(scheme)
+	_ = controlplanev1.AddToScheme(scheme)
+	_ = infrav1.AddToScheme(scheme)
+
+	// Register the RuntimeHook types into the catalog.
+	_ = runtimehooksv1.AddToCatalog(catalog)
 }
 
 // InitFlags initializes the flags.
@@ -83,27 +109,56 @@ func InitFlags(fs *pflag.FlagSet) {
 	// recommended because it helps in ensuring consistency across different components in the cluster.
 	logsv1.AddFlags(logOptions, fs)
 
-	// Add test-extension specific flags
-	// NOTE: it is not mandatory to use the same flag names in all RuntimeExtension, but it is recommended when
-	// addressing common concerns like profiler-address, webhook-port, webhook-cert-dir etc. because it helps in ensuring
-	// consistency across different components in the cluster.
+	fs.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+
+	fs.DurationVar(&leaderElectionLeaseDuration, "leader-elect-lease-duration", 15*time.Second,
+		"Interval at which non-leader candidates will wait to force acquire leadership (duration string)")
+
+	fs.DurationVar(&leaderElectionRenewDeadline, "leader-elect-renew-deadline", 10*time.Second,
+		"Duration that the leading controller manager will retry refreshing leadership before giving up (duration string)")
+
+	fs.DurationVar(&leaderElectionRetryPeriod, "leader-elect-retry-period", 2*time.Second,
+		"Duration the LeaderElector clients should wait between tries of actions (duration string)")
 
 	fs.StringVar(&profilerAddress, "profiler-address", "",
 		"Bind address to expose the pprof profiler (e.g. localhost:6060)")
+
+	fs.BoolVar(&enableContentionProfiling, "contention-profiling", false,
+		"Enable block profiling")
+
+	fs.DurationVar(&syncPeriod, "sync-period", 10*time.Minute,
+		"The minimum interval at which watched resources are reconciled (e.g. 15m)")
+
+	fs.Float32Var(&restConfigQPS, "kube-api-qps", 20,
+		"Maximum queries per second from the controller client to the Kubernetes API server. Defaults to 20")
+
+	fs.IntVar(&restConfigBurst, "kube-api-burst", 30,
+		"Maximum number of queries that should be allowed in one burst from the controller client to the Kubernetes API server. Default 30")
 
 	fs.IntVar(&webhookPort, "webhook-port", 9443,
 		"Webhook Server port")
 
 	fs.StringVar(&webhookCertDir, "webhook-cert-dir", "/tmp/k8s-webhook-server/serving-certs/",
 		"Webhook cert dir, only used when webhook-port is specified.")
+
+	fs.StringVar(&healthAddr, "health-addr", ":9440",
+		"The address the health endpoint binds to.")
+
+	flags.AddDiagnosticsOptions(fs, &diagnosticsOptions)
+	flags.AddTLSOptions(fs, &tlsOptions)
+
+	// Add test-extension specific flags
+	// NOTE: it is not mandatory to use the same flag names in all RuntimeExtension, but it is recommended when
+	// addressing common concerns like profiler-address, webhook-port, webhook-cert-dir etc. because it helps in ensuring
+	// consistency across different components in the cluster.
 }
 
-func main() {
-	// Creates a logger to be used during the main func using controller runtime utilities
-	// NOTE: it is not mandatory to use controller runtime utilities in custom RuntimeExtension, but it is recommended
-	// because it makes log from those components similar to log from controllers.
-	setupLog := ctrl.Log.WithName("main")
+// Add RBAC for the authorized diagnostics endpoint.
+// +kubebuilder:rbac:groups=authentication.k8s.io,resources=tokenreviews,verbs=create
+// +kubebuilder:rbac:groups=authorization.k8s.io,resources=subjectaccessreviews,verbs=create
 
+func main() {
 	// Initialize and parse command line flags.
 	InitFlags(pflag.CommandLine)
 	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
@@ -126,49 +181,98 @@ func main() {
 	// Add the klog logger in the context.
 	// NOTE: it is not mandatory to use contextual logging in custom RuntimeExtension, but it is recommended
 	// because it allows to use a log stored in the context across the entire chain of calls (without
-	// requiring an addition log parameter in all the functions.
+	// requiring an addition log parameter in all the functions).
 	ctrl.SetLogger(klog.Background())
 
-	// Initialize the golang profiler server, if required.
-	// TODO(sbueringer): remove this once we switched to using a manager.
-	if profilerAddress != "" {
-		setupLog.Info(fmt.Sprintf("Profiler listening for requests at %s", profilerAddress))
-		go func() {
-			srv := http.Server{Addr: profilerAddress, ReadHeaderTimeout: 2 * time.Second}
-			if err := srv.ListenAndServe(); err != nil {
-				setupLog.Error(err, "problem running profiler server")
-			}
-		}()
-	}
+	restConfig := ctrl.GetConfigOrDie()
+	restConfig.QPS = restConfigQPS
+	restConfig.Burst = restConfigBurst
+	restConfig.UserAgent = remote.DefaultClusterAPIUserAgent(controllerName)
 
-	// ****************************************************
-	//  Create a http server for serving runtime extensions
-	// ****************************************************
-
-	webhookServer, err := server.New(server.Options{
-		Catalog: catalog,
-		Port:    webhookPort,
-		CertDir: webhookCertDir,
-	})
+	tlsOptionOverrides, err := flags.GetTLSOptionOverrideFuncs(tlsOptions)
 	if err != nil {
-		setupLog.Error(err, "error creating webhook server")
+		setupLog.Error(err, "unable to add TLS settings to the webhook server")
 		os.Exit(1)
 	}
 
-	// ****************************************************
-	//  Add handlers for the Runtime hooks you are
-	//  interested in, in the test-extension we are registering all
-	//  of them for ensuring proper test coverage
-	// ****************************************************
+	diagnosticsOpts := flags.GetDiagnosticsOptions(diagnosticsOptions)
 
-	// Topology Mutation Hooks (Runtime Patches)
+	if enableContentionProfiling {
+		goruntime.SetBlockProfileRate(1)
+	}
 
+	// Create an HTTP server for serving Runtime Extensions.
+	runtimeExtensionWebhookServer, err := server.New(server.Options{
+		Port:    webhookPort,
+		CertDir: webhookCertDir,
+		TLSOpts: tlsOptionOverrides,
+		Catalog: catalog,
+	})
+	if err != nil {
+		setupLog.Error(err, "error creating runtime extension webhook server")
+		os.Exit(1)
+	}
+
+	ctrlOptions := ctrl.Options{
+		Scheme:                     scheme,
+		LeaderElection:             enableLeaderElection,
+		LeaderElectionID:           "controller-leader-election-capv-test-extension",
+		LeaseDuration:              &leaderElectionLeaseDuration,
+		RenewDeadline:              &leaderElectionRenewDeadline,
+		RetryPeriod:                &leaderElectionRetryPeriod,
+		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
+		HealthProbeBindAddress:     healthAddr,
+		PprofBindAddress:           profilerAddress,
+		Metrics:                    diagnosticsOpts,
+		Cache: cache.Options{
+			SyncPeriod: &syncPeriod,
+		},
+		Client: client.Options{
+			Cache: &client.CacheOptions{
+				DisableFor: []client.Object{
+					&corev1.ConfigMap{},
+					&corev1.Secret{},
+				},
+			},
+		},
+		WebhookServer: runtimeExtensionWebhookServer,
+	}
+
+	// Start the manager
+	mgr, err := ctrl.NewManager(restConfig, ctrlOptions)
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	// Set up a context listening for SIGINT.
+	ctx := ctrl.SetupSignalHandler()
+
+	// Setup Runtime Extensions.
+	setupTopologyMutationHookHandlers(runtimeExtensionWebhookServer)
+	setupLifecycleHookHandlers(mgr, runtimeExtensionWebhookServer)
+
+	// Setup checks, indexes, reconcilers and webhooks.
+	setupChecks(mgr)
+	setupIndexes(ctx, mgr)
+	setupReconcilers(ctx, mgr)
+	setupWebhooks(mgr)
+
+	setupLog.Info("Starting manager", "version", version.Get().String())
+	if err := mgr.Start(ctx); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
+
+// setupTopologyMutationHookHandlers sets up Topology Mutation Hooks (Runtime Patches).
+func setupTopologyMutationHookHandlers(runtimeExtensionWebhookServer *server.Server) {
 	// Create the ExtensionHandlers for the Topology Mutation Hooks.
 	// NOTE: it is not mandatory to group all the ExtensionHandlers using a struct, what is important
 	// is to have HandlerFunc with the signature defined in sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1.
 	topologyMutationExtensionHandlers := topologymutation.NewExtensionHandlers(scheme)
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.GeneratePatches,
 		Name:        "generate-patches",
 		HandlerFunc: topologyMutationExtensionHandlers.GeneratePatches,
@@ -177,7 +281,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.ValidateTopology,
 		Name:        "validate-topology",
 		HandlerFunc: topologyMutationExtensionHandlers.ValidateTopology,
@@ -186,7 +290,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.DiscoverVariables,
 		Name:        "discover-variables",
 		HandlerFunc: topologyMutationExtensionHandlers.DiscoverVariables,
@@ -194,30 +298,16 @@ func main() {
 		setupLog.Error(err, "error adding handler")
 		os.Exit(1)
 	}
+}
 
-	// Lifecycle Hooks
-
-	// Gets a client to access the Kubernetes cluster where this RuntimeExtension will be deployed to;
-	// this is a requirement specific of the lifecycle hooks implementation for Cluster APIs E2E tests.
-	restConfig, err := ctrl.GetConfig()
-	if err != nil {
-		setupLog.Error(err, "error getting config for the cluster")
-		os.Exit(1)
-	}
-	restConfig.UserAgent = remote.DefaultClusterAPIUserAgent(controllerName)
-
-	client, err := client.New(restConfig, client.Options{})
-	if err != nil {
-		setupLog.Error(err, "error creating client to the cluster")
-		os.Exit(1)
-	}
-
+// setupLifecycleHookHandlers sets up Lifecycle Hooks.
+func setupLifecycleHookHandlers(mgr ctrl.Manager, runtimeExtensionWebhookServer *server.Server) {
 	// Create the ExtensionHandlers for the lifecycle hooks
 	// NOTE: it is not mandatory to group all the ExtensionHandlers using a struct, what is important
 	// is to have HandlerFunc with the signature defined in sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1.
-	lifecycleExtensionHandlers := lifecycle.NewExtensionHandlers(client)
+	lifecycleExtensionHandlers := lifecycle.NewExtensionHandlers(mgr.GetClient())
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.BeforeClusterCreate,
 		Name:        "before-cluster-create",
 		HandlerFunc: lifecycleExtensionHandlers.DoBeforeClusterCreate,
@@ -226,7 +316,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.AfterControlPlaneInitialized,
 		Name:        "after-control-plane-initialized",
 		HandlerFunc: lifecycleExtensionHandlers.DoAfterControlPlaneInitialized,
@@ -235,7 +325,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.BeforeClusterUpgrade,
 		Name:        "before-cluster-upgrade",
 		HandlerFunc: lifecycleExtensionHandlers.DoBeforeClusterUpgrade,
@@ -244,7 +334,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.AfterControlPlaneUpgrade,
 		Name:        "after-control-plane-upgrade",
 		HandlerFunc: lifecycleExtensionHandlers.DoAfterControlPlaneUpgrade,
@@ -253,7 +343,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.AfterClusterUpgrade,
 		Name:        "after-cluster-upgrade",
 		HandlerFunc: lifecycleExtensionHandlers.DoAfterClusterUpgrade,
@@ -262,7 +352,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := webhookServer.AddExtensionHandler(server.ExtensionHandler{
+	if err := runtimeExtensionWebhookServer.AddExtensionHandler(server.ExtensionHandler{
 		Hook:        runtimehooksv1.BeforeClusterDelete,
 		Name:        "before-cluster-delete",
 		HandlerFunc: lifecycleExtensionHandlers.DoBeforeClusterDelete,
@@ -270,17 +360,25 @@ func main() {
 		setupLog.Error(err, "error adding handler")
 		os.Exit(1)
 	}
+}
 
-	// ****************************************************
-	//  Start the https server
-	// ****************************************************
-
-	// Setup a context listening for SIGINT.
-	ctx := ctrl.SetupSignalHandler()
-
-	setupLog.Info("starting RuntimeExtension", "version", version.Get().String())
-	if err := webhookServer.Start(ctx); err != nil {
-		setupLog.Error(err, "error running webhook server")
+func setupChecks(mgr ctrl.Manager) {
+	if err := mgr.AddReadyzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
+		setupLog.Error(err, "unable to create ready check")
 		os.Exit(1)
 	}
+
+	if err := mgr.AddHealthzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
+		setupLog.Error(err, "unable to create health check")
+		os.Exit(1)
+	}
+}
+
+func setupIndexes(_ context.Context, _ ctrl.Manager) {
+}
+
+func setupReconcilers(_ context.Context, _ ctrl.Manager) {
+}
+
+func setupWebhooks(_ ctrl.Manager) {
 }

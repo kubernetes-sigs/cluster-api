26d08b1b7317e9731e1925e14456f44328221ad9
Jesse Hu <hui.hu@smartx.com>
2024-04-05 16:28:30 +0800

    rename func to UnhealthyMachinesWithMissingNodeOrUnhealthyControlPlaneComponents

--- controlplane/kubeadm/internal/controllers/scale.go
+++ controlplane/kubeadm/internal/controllers/scale.go
@@ -230,8 +230,8 @@ func selectMachineForScaleDown(ctx context.Context, controlPlane *internal.Contr
 		machines = controlPlane.MachineWithDeleteAnnotation(outdatedMachines)
 	case controlPlane.MachineWithDeleteAnnotation(machines).Len() > 0:
 		machines = controlPlane.MachineWithDeleteAnnotation(machines)
-	case controlPlane.UnhealthyControlPlaneOrNodeMissingMachines(outdatedMachines).Len() > 0:
-		machines = controlPlane.UnhealthyControlPlaneOrNodeMissingMachines(outdatedMachines)
+	case controlPlane.UnhealthyMachinesWithMissingNodeOrUnhealthyControlPlaneComponents(outdatedMachines).Len() > 0:
+		machines = controlPlane.UnhealthyMachinesWithMissingNodeOrUnhealthyControlPlaneComponents(outdatedMachines)
 	case outdatedMachines.Len() > 0:
 		machines = outdatedMachines
 	}

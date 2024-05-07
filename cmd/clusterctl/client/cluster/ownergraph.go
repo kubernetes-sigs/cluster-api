512dc36da7fa818f7636c18c0b04342c5c59e511
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2024-05-07 06:14:55 -0700

    [release-1.7] 🐛 test: filter cluster-wide objects asserted in ResourceVersion tests to exclude objects of parallel tests (#10570)
    * test: filter cluster-wide objects asserted in ResourceVersion tests to exclude objects of parallel tests
    
    * review fixes
    
    * review fixes
    
    ---------
    
    Co-authored-by: Christian Schlotter <christian.schlotter@broadcom.com>

--- cmd/clusterctl/client/cluster/ownergraph.go
+++ cmd/clusterctl/client/cluster/ownergraph.go
@@ -39,11 +39,35 @@ type OwnerGraphNode struct {
 	Owners []metav1.OwnerReference
 }
 
+// GetOwnerGraphFilterFunction allows filtering the objects returned by GetOwnerGraph.
+// The function has to return true for objects which should be kept.
+// NOTE: this function signature is exposed to allow implementation of E2E tests; there is
+// no guarantee about the stability of this API.
+type GetOwnerGraphFilterFunction func(u unstructured.Unstructured) bool
+
+// FilterClusterObjectsWithNameFilter is used in e2e tests where the owner graph
+// gets queried to filter out cluster-wide objects which don't have the s in their
+// object name. This avoids assertions on objects which are part of in-parallel
+// running tests like ExtensionConfig.
+// NOTE: this function signature is exposed to allow implementation of E2E tests; there is
+// no guarantee about the stability of this API.
+func FilterClusterObjectsWithNameFilter(s string) func(u unstructured.Unstructured) bool {
+	return func(u unstructured.Unstructured) bool {
+		// Ignore cluster-wide objects which don't have the clusterName in their object
+		// name to avoid asserting on cluster-wide objects which get created or deleted
+		// by tests which run in-parallel (e.g. ExtensionConfig).
+		if u.GetNamespace() == "" && !strings.Contains(u.GetName(), s) {
+			return false
+		}
+		return true
+	}
+}
+
 // GetOwnerGraph returns a graph with all the objects considered by clusterctl move as nodes and the OwnerReference relationship between those objects as edges.
 // NOTE: this data structure is exposed to allow implementation of E2E tests verifying that CAPI can properly rebuild its
 // own owner references; there is no guarantee about the stability of this API. Using this test with providers may require
 // a custom implementation of this function, or the OwnerGraph it returns.
-func GetOwnerGraph(ctx context.Context, namespace, kubeconfigPath string) (OwnerGraph, error) {
+func GetOwnerGraph(ctx context.Context, namespace, kubeconfigPath string, filterFn GetOwnerGraphFilterFunction) (OwnerGraph, error) {
 	p := newProxy(Kubeconfig{Path: kubeconfigPath, Context: ""})
 	invClient := newInventoryClient(p, nil)
 
@@ -57,14 +81,14 @@ func GetOwnerGraph(ctx context.Context, namespace, kubeconfigPath string) (Owner
 
 	// graph.Discovery can not be used here as it will use the latest APIVersion for ownerReferences - not those
 	// present in the object `metadata.ownerReferences`.
-	owners, err := discoverOwnerGraph(ctx, namespace, graph)
+	owners, err := discoverOwnerGraph(ctx, namespace, graph, filterFn)
 	if err != nil {
 		return OwnerGraph{}, errors.Wrap(err, "failed to discovery ownerGraph types")
 	}
 	return owners, nil
 }
 
-func discoverOwnerGraph(ctx context.Context, namespace string, o *objectGraph) (OwnerGraph, error) {
+func discoverOwnerGraph(ctx context.Context, namespace string, o *objectGraph, filterFn GetOwnerGraphFilterFunction) (OwnerGraph, error) {
 	selectors := []client.ListOption{}
 	if namespace != "" {
 		selectors = append(selectors, client.InNamespace(namespace))
@@ -102,6 +126,10 @@ func discoverOwnerGraph(ctx context.Context, namespace string, o *objectGraph) (
 			}
 		}
 		for _, obj := range objList.Items {
+			// Exclude the objects via the filter function.
+			if filterFn != nil && !filterFn(obj) {
+				continue
+			}
 			// Exclude the kube-root-ca.crt ConfigMap from the owner graph.
 			if obj.GetKind() == "ConfigMap" && obj.GetName() == "kube-root-ca.crt" {
 				continue

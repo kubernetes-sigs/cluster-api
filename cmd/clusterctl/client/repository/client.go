639230a03e6b2dda6a7faa3a33a1f8662e493dba
dongheon.lee <aetertinas@gmail.com>
2024-05-14 22:11:48 +0900

    GitLab can use both Path and RawPath

--- cmd/clusterctl/client/repository/client.go
+++ cmd/clusterctl/client/repository/client.go
@@ -186,7 +186,7 @@ func repositoryFactory(ctx context.Context, providerConfig config.Provider, conf
 		}
 
 		// if the url is a GitLab repository
-		if strings.HasPrefix(rURL.Host, gitlabHostPrefix) && strings.HasPrefix(rURL.RawPath, gitlabPackagesAPIPrefix) {
+		if strings.HasPrefix(rURL.Host, gitlabHostPrefix) && strings.HasPrefix(rURL.EscapedPath(), gitlabPackagesAPIPrefix) {
 			repo, err := NewGitLabRepository(providerConfig, configVariablesClient)
 			if err != nil {
 				return nil, errors.Wrap(err, "error creating the GitLab repository client")

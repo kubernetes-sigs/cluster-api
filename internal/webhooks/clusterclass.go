28540f80fc1aaf3cafd44172ff9e63129960c7df
Stefan Bueringer <buringerst@vmware.com>
2024-05-24 10:35:30 +0200

    Fix indexing in ClusterClass webhook MHC validation
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- internal/webhooks/clusterclass.go
+++ internal/webhooks/clusterclass.go
@@ -411,7 +411,7 @@ func validateMachineHealthCheckClasses(clusterClass *clusterv1.ClusterClass) fie
 		if md.MachineHealthCheck == nil {
 			continue
 		}
-		fldPath := field.NewPath("spec", "workers", "machineDeployments", "machineHealthCheck").Index(i)
+		fldPath := field.NewPath("spec", "workers", "machineDeployments").Index(i).Child("machineHealthCheck")
 
 		allErrs = append(allErrs, validateMachineHealthCheckClass(fldPath, clusterClass.Namespace, md.MachineHealthCheck)...)
 	}

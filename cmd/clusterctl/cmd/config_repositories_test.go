1c37f25c1626d190fd1586a497bba438aae54031
Ludwig <ludwig.bedacht@gmail.com>
2024-05-31 09:59:46 +0200

    add ionoscloud provider

--- cmd/clusterctl/cmd/config_repositories_test.go
+++ cmd/clusterctl/cmd/config_repositories_test.go
@@ -133,6 +133,7 @@ hetzner                 InfrastructureProvider   https://github.com/syself/clust
 hivelocity-hivelocity   InfrastructureProvider   https://github.com/hivelocity/cluster-api-provider-hivelocity/releases/latest/              infrastructure-components.yaml
 ibmcloud                InfrastructureProvider   https://github.com/kubernetes-sigs/cluster-api-provider-ibmcloud/releases/latest/           infrastructure-components.yaml
 in-memory               InfrastructureProvider   https://github.com/kubernetes-sigs/cluster-api/releases/latest/                             infrastructure-components-in-memory-development.yaml
+ionoscloud              InfrastructureProvider   https://github.com/ionos-cloud/cluster-api-provider-ionoscloud/releases/latest/             infrastructure-components.yaml
 k0sproject-k0smotron    InfrastructureProvider   https://github.com/k0sproject/k0smotron/releases/latest/                                    infrastructure-components.yaml
 kubekey                 InfrastructureProvider   https://github.com/kubesphere/kubekey/releases/latest/                                      infrastructure-components.yaml
 kubevirt                InfrastructureProvider   https://github.com/kubernetes-sigs/cluster-api-provider-kubevirt/releases/latest/           infrastructure-components.yaml
@@ -277,6 +278,10 @@ var expectedOutputYaml = `- File: core_components.yaml
   Name: in-memory
   ProviderType: InfrastructureProvider
   URL: https://github.com/kubernetes-sigs/cluster-api/releases/latest/
+- File: infrastructure-components.yaml
+  Name: ionoscloud
+  ProviderType: InfrastructureProvider
+  URL: https://github.com/ionos-cloud/cluster-api-provider-ionoscloud/releases/latest/
 - File: infrastructure-components.yaml
   Name: k0sproject-k0smotron
   ProviderType: InfrastructureProvider

4682c6b839e5a6e0d05344743ea9eb024ec20404
Stefan Bueringer <buringerst@vmware.com>
2024-08-15 13:04:43 +0200

    Ensure TODOs in API types are single line
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- bootstrap/kubeadm/types/upstreamv1beta3/types.go
+++ bootstrap/kubeadm/types/upstreamv1beta3/types.go
@@ -138,8 +138,7 @@ type ClusterConfiguration struct {
 // ControlPlaneComponent holds settings common to control plane component of the cluster.
 type ControlPlaneComponent struct {
 	// ExtraArgs is an extra set of flags to pass to the control plane component.
-	// TODO: This is temporary and ideally we would like to switch all components to
-	// use ComponentConfig + ConfigMaps.
+	// TODO: This is temporary and ideally we would like to switch all components to use ComponentConfig + ConfigMaps.
 	// +optional
 	ExtraArgs map[string]string `json:"extraArgs,omitempty"`
 

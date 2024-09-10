8b3fab1b60be2598a6f6f9afd2e24a74d2dcb6fa
mondragonfx <35617339+mondragonfx@users.noreply.github.com>
2024-09-10 14:42:17 -0400

    :seedling: Add vultr infrastructure provider (#11091)
    * add vultr provider
    
    Signed-off-by: cmondragon <cmondragon@vultr.com>
    
    * fix indent
    
    * Fix indentation consistency
    
    * add github org name
    
    * gofmt
    
    * update provider name
    
    * fix provider expectedOutput
    
    ---------
    
    Signed-off-by: cmondragon <cmondragon@vultr.com>

--- cmd/clusterctl/cmd/config_repositories_test.go
+++ cmd/clusterctl/cmd/config_repositories_test.go
@@ -154,6 +154,7 @@ vcd                     InfrastructureProvider   https://github.com/vmware/clust
 vcluster                InfrastructureProvider   https://github.com/loft-sh/cluster-api-provider-vcluster/releases/latest/                   infrastructure-components.yaml
 virtink                 InfrastructureProvider   https://github.com/smartxworks/cluster-api-provider-virtink/releases/latest/                infrastructure-components.yaml
 vsphere                 InfrastructureProvider   https://github.com/kubernetes-sigs/cluster-api-provider-vsphere/releases/latest/            infrastructure-components.yaml
+vultr-vultr             InfrastructureProvider   https://github.com/vultr/cluster-api-provider-vultr/releases/latest/                        infrastructure-components.yaml
 in-cluster              IPAMProvider             https://github.com/kubernetes-sigs/cluster-api-ipam-provider-in-cluster/releases/latest/    ipam-components.yaml
 helm                    AddonProvider            https://github.com/kubernetes-sigs/cluster-api-addon-provider-helm/releases/latest/         addon-components.yaml
 `
@@ -362,6 +363,10 @@ var expectedOutputYaml = `- File: core_components.yaml
   Name: vsphere
   ProviderType: InfrastructureProvider
   URL: https://github.com/kubernetes-sigs/cluster-api-provider-vsphere/releases/latest/
+- File: infrastructure-components.yaml
+  Name: vultr-vultr
+  ProviderType: InfrastructureProvider
+  URL: https://github.com/vultr/cluster-api-provider-vultr/releases/latest/
 - File: ipam-components.yaml
   Name: in-cluster
   ProviderType: IPAMProvider

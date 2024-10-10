5dca2541032c3504322e7ce7e37b5c2b9f389a94
Christian Schlotter <christian.schlotter@broadcom.com>
2024-10-10 16:24:46 +0200

    v1beta2: start on the conditions

--- internal/controllers/machineset/machineset_status.go
+++ internal/controllers/machineset/machineset_status.go
@@ -18,8 +18,182 @@ package machineset
 
 import (
 	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	v1beta2conditions "sigs.k8s.io/cluster-api/util/conditions/v1beta2"
 )
 
-func (r *Reconciler) reconcileStatus(ctx context.Context, s *scope) {
+// reconcileStatus reconciles Machine's status during the entire lifecycle of the machine.
+// This implies that the code in this function should account for several edge cases e.g. machine being partially provisioned,
+// machine being partially deleted but also for running machines being disrupted e.g. by deleting the node.
+// Additionally, this func should ensure that the conditions managed by this controller are always set in order to
+// comply with the recommendation in the Kubernetes API guidelines.
+// Note: v1beta1 conditions are not managed by this func.
+func (r *Reconciler) reconcileStatus(_ context.Context, s *scope) {
+	// Update the following fields in status from the machines list.
+	// - ReadyReplicas
+	// - AvailableReplicas
+	// Also updates the following conditions:
+	// - ScalingUp
+	// - ScalingDown
+	setReplicas(s.machineSet, s.machines)
+	setScalingUpCondition(s.machineSet, s.machines)
+	setScalingDownCondition(s.machineSet, s.machines)
 
+	// Conditions
+
+	// MachinesReady
+	if err := v1beta2conditions.SetAggregateCondition(s.machines, s.machineSet,
+		clusterv1.MachineReadyV1Beta2Condition,
+		v1beta2conditions.TargetConditionType(clusterv1.MachineSetMachinesReadyV1Beta2Condition),
+	); err != nil {
+		v1beta2conditions.Set(s.machineSet, metav1.Condition{
+			Type:    clusterv1.MachineSetMachinesReadyV1Beta2Condition,
+			Status:  metav1.ConditionFalse,
+			Reason:  "TODOFailedDuringAggregation",
+			Message: "something TODO at MachinesReady",
+		})
+	}
+
+	// MachinesUpToDate
+	if err := v1beta2conditions.SetAggregateCondition(s.machines, s.machineSet,
+		clusterv1.MachinesUpToDateV1Beta2Condition,
+		v1beta2conditions.TargetConditionType(clusterv1.MachineSetMachinesUpToDateV1Beta2Condition),
+	); err != nil {
+		v1beta2conditions.Set(s.machineSet, metav1.Condition{
+			Type:    clusterv1.MachineSetMachinesUpToDateV1Beta2Condition,
+			Status:  metav1.ConditionFalse,
+			Reason:  "TODOFailedDuringAggregation",
+			Message: "something TODO on MachinesUpToDate",
+		})
+	}
+
+	// Paused
+	// TODO(chrischdi): setPausedCondition(s.machineSet)
+
+	// Deleting
+	setDeletingCondition(s.machineSet)
+}
+
+func setReplicas(ms *clusterv1.MachineSet, machines []*clusterv1.Machine) {
+	if machines == nil {
+		// TODO: what about replicas fields? Not touch?!
+		return
+	}
+
+	var readyReplicas, availableReplicas int32
+	for _, machine := range machines {
+		if meta.IsStatusConditionTrue(machine.GetV1Beta2Conditions(), clusterv1.MachineReadyV1Beta2Condition) {
+			readyReplicas++
+		}
+		if meta.IsStatusConditionTrue(machine.GetV1Beta2Conditions(), clusterv1.MachineAvailableV1Beta2Condition) {
+			availableReplicas++
+		}
+	}
+
+	ms.Status.V1Beta2.ReadyReplicas = ptr.To(readyReplicas)
+	ms.Status.V1Beta2.AvailableReplicas = ptr.To(availableReplicas)
+}
+
+func setScalingUpCondition(ms *clusterv1.MachineSet, machines []*clusterv1.Machine) {
+	if machines == nil {
+		// Question: should we only do this if the condition does not exist?
+		v1beta2conditions.Set(ms, metav1.Condition{
+			Type:    clusterv1.MachineSetScalingUpV1Beta2Condition,
+			Status:  metav1.ConditionUnknown,
+			Reason:  "InternalError", // TODO: create a const.
+			Message: "Please check controller logs for errors",
+		})
+		return
+	}
+
+	desiredReplicas := *ms.Spec.Replicas
+	availableReplicas := *ms.Status.V1Beta2.AvailableReplicas
+
+	if availableReplicas < desiredReplicas {
+		v1beta2conditions.Set(ms, metav1.Condition{
+			Type:    clusterv1.MachineSetScalingUpV1Beta2Condition,
+			Status:  metav1.ConditionTrue,
+			Reason:  "NotAllAvailable", // TODO: create a const.
+			Message: fmt.Sprintf("The MachineSet currently has %d/%d available replicas", availableReplicas, desiredReplicas),
+		})
+		return
+	}
+
+	v1beta2conditions.Set(ms, metav1.Condition{
+		Type:   clusterv1.MachineSetScalingUpV1Beta2Condition,
+		Status: metav1.ConditionFalse,
+		Reason: "NotScalingUp", // TODO: create a const.
+	})
+}
+
+func setScalingDownCondition(ms *clusterv1.MachineSet, machines []*clusterv1.Machine) {
+	if machines == nil {
+		// Question: should we only do this if the condition does not exist?
+		v1beta2conditions.Set(ms, metav1.Condition{
+			Type:    clusterv1.MachineSetScalingDownV1Beta2Condition,
+			Status:  metav1.ConditionUnknown,
+			Reason:  "InternalError", // TODO: create a const.
+			Message: "Please check controller logs for errors",
+		})
+
+		// TODO: what about replicas fields? Not touch?
+		return
+	}
+
+	desiredReplicas := *ms.Spec.Replicas
+
+	if int32(len(machines)) <= (desiredReplicas) {
+		v1beta2conditions.Set(ms, metav1.Condition{
+			Type:   clusterv1.MachineSetScalingDownV1Beta2Condition,
+			Status: metav1.ConditionFalse,
+			Reason: "NotScalingDown", // TODO: create a const.
+		})
+		return
+	}
+
+	if !ms.DeletionTimestamp.IsZero() {
+		v1beta2conditions.Set(ms, metav1.Condition{
+			Type:    clusterv1.MachineSetScalingDownV1Beta2Condition,
+			Status:  metav1.ConditionTrue,
+			Reason:  "Deleting", // TODO: create a const.
+			Message: "The MachineSet is in deletion",
+		})
+		return
+	}
+	v1beta2conditions.Set(ms, metav1.Condition{
+		Type:    clusterv1.MachineSetScalingDownV1Beta2Condition,
+		Status:  metav1.ConditionTrue,
+		Reason:  "TooManyReplicas", // TODO: create a const.
+		Message: fmt.Sprintf("The MachineSet currently has %d replicas but should only have %d", len(machines), desiredReplicas),
+	})
+}
+
+// func setPausedCondition(ms *clusterv1.MachineSet) {
+// 	v1beta2conditions.Set(ms, metav1.Condition{
+// 		Type:   clusterv1.MachineSetPausedV1Beta2Condition,
+// 		Status: metav1.ConditionFalse,
+// 		Reason: "NotPaused", // TODO: create a const.
+// 	})
+// }
+
+func setDeletingCondition(ms *clusterv1.MachineSet) {
+	if !ms.DeletionTimestamp.IsZero() {
+		v1beta2conditions.Set(ms, metav1.Condition{
+			Type:   clusterv1.MachineSetDeletingV1Beta2Condition,
+			Status: metav1.ConditionTrue,
+			Reason: "DeletionTimestampSet", // TODO: create a const.
+		})
+		return
+	}
+	v1beta2conditions.Set(ms, metav1.Condition{
+		Type:   clusterv1.MachineSetDeletingV1Beta2Condition,
+		Status: metav1.ConditionFalse,
+		Reason: "NoDeletionTimestamp", // TODO: create a const.
+	})
 }

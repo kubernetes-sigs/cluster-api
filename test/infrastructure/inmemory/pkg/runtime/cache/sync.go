214ab6dcfafc3a0c0088971304c94f10c96f3ea8
sivchari <shibuuuu5@gmail.com>
2024-10-23 17:52:52 +0900

    :bug: Add race option to detect raced codes (#10899)
    * fix: race code
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>
    
    * remove sync variable
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>
    
    * use sync chan
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>
    
    * drop chan
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>
    
    * add -race option again
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>
    
    * use atomic package
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>
    
    ---------
    
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- test/infrastructure/inmemory/pkg/runtime/cache/sync.go
+++ test/infrastructure/inmemory/pkg/runtime/cache/sync.go
@@ -50,10 +50,10 @@ func (c *cache) startSyncer(ctx context.Context) error {
 		c.syncQueue.ShutDown()
 	}()
 
-	syncLoopStarted := false
+	var syncLoopStarted atomic.Bool
 	go func() {
 		log.Info("Starting sync loop")
-		syncLoopStarted = true
+		syncLoopStarted.Store(true)
 		for {
 			select {
 			case <-time.After(c.syncPeriod / 4):
@@ -63,15 +63,14 @@ func (c *cache) startSyncer(ctx context.Context) error {
 			}
 		}
 	}()
-
-	var workers int64
+	var workers atomic.Int64
 	go func() {
 		log.Info("Starting sync workers", "count", c.syncConcurrency)
 		wg := &sync.WaitGroup{}
 		wg.Add(c.syncConcurrency)
 		for range c.syncConcurrency {
 			go func() {
-				atomic.AddInt64(&workers, 1)
+				workers.Add(1)
 				defer wg.Done()
 				for c.processSyncWorkItem(ctx) {
 				}
@@ -82,7 +81,7 @@ func (c *cache) startSyncer(ctx context.Context) error {
 	}()
 
 	if err := wait.PollUntilContextTimeout(ctx, 50*time.Millisecond, 5*time.Second, false, func(context.Context) (done bool, err error) {
-		if !syncLoopStarted {
+		if !syncLoopStarted.Load() {
 			return false, nil
 		}
 		return true, nil
@@ -91,7 +90,7 @@ func (c *cache) startSyncer(ctx context.Context) error {
 	}
 
 	if err := wait.PollUntilContextTimeout(ctx, 50*time.Millisecond, 5*time.Second, false, func(context.Context) (done bool, err error) {
-		if atomic.LoadInt64(&workers) < int64(c.syncConcurrency) {
+		if workers.Load() < int64(c.syncConcurrency) {
 			return false, nil
 		}
 		return true, nil

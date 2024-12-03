f989ec1706e9ecea62a830edd8bd56ccb05b324d
Christian Schlotter <chrischdi@users.noreply.github.com>
2024-12-03 19:35:02 +0100

    test: add options for additional resources and verify volume detach to node drain test (#11526)

--- test/e2e/node_drain_test.go
+++ test/e2e/node_drain_test.go
@@ -20,8 +20,18 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
+	"fmt"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/cluster-api/test/framework"
 )
 
 var _ = Describe("When testing Node drain", func() {
@@ -34,6 +44,40 @@ var _ = Describe("When testing Node drain", func() {
 			SkipCleanup:            skipCleanup,
 			Flavor:                 ptr.To("topology"),
 			InfrastructureProvider: ptr.To("docker"),
+			VerifyNodeVolumeDetach: true,
+			CreateAdditionalResources: func(ctx context.Context, clusterProxy framework.ClusterProxy, cluster *clusterv1.Cluster) {
+				workloadClusterClient := clusterProxy.GetWorkloadCluster(ctx, cluster.Namespace, cluster.Name).GetClient()
+
+				nodeList := &corev1.NodeList{}
+				Expect(workloadClusterClient.List(ctx, nodeList)).To(Succeed())
+
+				// Create a fake VolumeAttachment object for each Node without having a real backing csi driver.
+				for _, node := range nodeList.Items {
+					va := generateVolumeAttachment(node)
+					Expect(workloadClusterClient.Create(ctx, va)).To(Succeed())
+					// Set .Status.Attached to true to make the VolumeAttachment blocking for machine deletions.
+					va.Status.Attached = true
+					Expect(workloadClusterClient.Status().Update(ctx, va)).To(Succeed())
+				}
+			},
 		}
 	})
 })
+
+func generateVolumeAttachment(node corev1.Node) *storagev1.VolumeAttachment {
+	return &storagev1.VolumeAttachment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("va-%s", node.GetName()),
+			Finalizers: []string{
+				"test.cluster.x-k8s.io/block",
+			},
+		},
+		Spec: storagev1.VolumeAttachmentSpec{
+			Attacher: "manual",
+			NodeName: node.GetName(),
+			Source: storagev1.VolumeAttachmentSource{
+				PersistentVolumeName: ptr.To("foo"),
+			},
+		},
+	}
+}

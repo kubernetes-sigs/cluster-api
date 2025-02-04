98e8910dd282500c31be5a1383d2054bb444d421
Christian Schlotter <christian.schlotter@broadcom.com>
2025-02-04 13:49:28 +0100

    e2e: drop v1.24 skip for runtime sdk test

--- test/e2e/cluster_upgrade_runtimesdk_test.go
+++ test/e2e/cluster_upgrade_runtimesdk_test.go
@@ -20,9 +20,7 @@ limitations under the License.
 package e2e
 
 import (
-	"github.com/blang/semver/v4"
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"k8s.io/utils/ptr"
 
 	clusterctlcluster "sigs.k8s.io/cluster-api/cmd/clusterctl/client/cluster"
@@ -31,12 +29,6 @@ import (
 
 var _ = Describe("When upgrading a workload cluster using ClusterClass with RuntimeSDK [ClusterClass]", Label("ClusterClass"), func() {
 	ClusterUpgradeWithRuntimeSDKSpec(ctx, func() ClusterUpgradeWithRuntimeSDKSpecInput {
-		version, err := semver.ParseTolerant(e2eConfig.MustGetVariable(KubernetesVersionUpgradeFrom))
-		Expect(err).ToNot(HaveOccurred(), "Invalid argument, KUBERNETES_VERSION_UPGRADE_FROM is not a valid version")
-		if version.LT(semver.MustParse("1.24.0")) {
-			Fail("This test only supports upgrades from Kubernetes >= v1.24.0")
-		}
-
 		return ClusterUpgradeWithRuntimeSDKSpecInput{
 			E2EConfig:              e2eConfig,
 			ClusterctlConfigPath:   clusterctlConfigPath,

90bc04b78c6bf29e36d299260f7cde737fc20ed5
sivchari <shibuuuu5@gmail.com>
2025-02-24 19:36:16 +0900

    fix semver
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- test/infrastructure/kind/mapper.go
+++ test/infrastructure/kind/mapper.go
@@ -79,27 +79,28 @@ type Mapping struct {
 var preBuiltMappings = []Mapping{
 
 	// TODO: Add pre-built images for newer Kind versions on top
-	// Pre-built images for Kind v1.26.
 	{
-		KubernetesVersion: semver.MustParse("v1.32.2"),
+		KubernetesVersion: semver.MustParse("1.32.2"),
 		Mode:              Mode0_20,
 		Image:             "kindest/node:v1.32.2@sha256:f226345927d7e348497136874b6d207e0b32cc52154ad8323129352923a3142f",
 	},
 	{
-		KubernetesVersion: semver.MustParse("v1.31.6"),
+		KubernetesVersion: semver.MustParse("1.31.6"),
 		Mode:              Mode0_20,
 		Image:             "kindest/node:v1.30.6@sha256:28b7cbb993dfe093c76641a0c95807637213c9109b761f1d422c2400e22b8e87",
 	},
 	{
-		KubernetesVersion: semver.MustParse("v1.30.10"),
+		KubernetesVersion: semver.MustParse("1.30.10"),
 		Mode:              Mode0_20,
 		Image:             "kindest/node:v1.30.10@sha256:4de75d0e82481ea846c0ed1de86328d821c1e6a6a91ac37bf804e5313670e507",
 	},
 	{
-		KubernetesVersion: semver.MustParse("v1.29.14"),
+		KubernetesVersion: semver.MustParse("1.29.14"),
 		Mode:              Mode0_20,
 		Image:             "kindest/node:v1.29.14@sha256:8703bd94ee24e51b778d5556ae310c6c0fa67d761fae6379c8e0bb480e6fea29",
 	},
+
+	// Pre-built images for Kind v1.26.
 	{
 		KubernetesVersion: semver.MustParse("1.32.0"),
 		Mode:              Mode0_20,

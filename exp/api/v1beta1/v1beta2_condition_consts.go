3c984ec77739938d5cb503149a33bfce509c80d8
Christian Schlotter <christian.schlotter@broadcom.com>
2025-04-15 14:01:15 +0200

    fixup machinepool v1beta2 conditions in v1beta1 package

--- exp/api/v1beta1/v1beta2_condition_consts.go
+++ exp/api/v1beta1/v1beta2_condition_consts.go
@@ -24,30 +24,30 @@ counters at cluster level is already implemented.
 // Conditions that will be used for the MachinePool object in v1Beta2 API version.
 const (
 	// MachinePoolAvailableV1Beta2Condition is true when InfrastructureReady and available replicas >= desired replicas.
-	MachinePoolAvailableV1Beta2Condition = clusterv1.AvailableCondition
+	MachinePoolAvailableV1Beta2Condition = clusterv1beta1.AvailableV1Beta2Condition
 
 	// MachinePoolBootstrapConfigReadyV1Beta2Condition mirrors the corresponding condition from the MachinePool's BootstrapConfig resource.
-	MachinePoolBootstrapConfigReadyV1Beta2Condition = clusterv1.BootstrapConfigReadyV1Beta2Condition
+	MachinePoolBootstrapConfigReadyV1Beta2Condition = clusterv1beta1.BootstrapConfigReadyV1Beta2Condition
 
 	// MachinePoolInfrastructureReadyV1Beta2Condition mirrors the corresponding condition from the MachinePool's Infrastructure resource.
-	MachinePoolInfrastructureReadyV1Beta2Condition = clusterv1.InfrastructureReadyV1Beta2Condition
+	MachinePoolInfrastructureReadyV1Beta2Condition = clusterv1beta1.InfrastructureReadyV1Beta2Condition
 
 	// MachinePoolMachinesReadyV1Beta2Condition surfaces detail of issues on the controlled machines, if any.
-	MachinePoolMachinesReadyV1Beta2Condition = clusterv1.MachinesReadyV1Beta2Condition
+	MachinePoolMachinesReadyV1Beta2Condition = clusterv1beta1.MachinesReadyV1Beta2Condition
 
 	// MachinePoolMachinesUpToDateV1Beta2Condition surfaces details of controlled machines not up to date, if any.
-	MachinePoolMachinesUpToDateV1Beta2Condition = clusterv1.MachinesUpToDateV1Beta2Condition
+	MachinePoolMachinesUpToDateV1Beta2Condition = clusterv1beta1.MachinesUpToDateV1Beta2Condition
 
 	// MachinePoolScalingUpV1Beta2Condition is true if available replicas < desired replicas.
-	MachinePoolScalingUpV1Beta2Condition = clusterv1.ScalingUpV1Beta2Condition
+	MachinePoolScalingUpV1Beta2Condition = clusterv1beta1.ScalingUpV1Beta2Condition
 
 	// MachinePoolScalingDownV1Beta2Condition is true if replicas > desired replicas.
-	MachinePoolScalingDownV1Beta2Condition = clusterv1.ScalingDownV1Beta2Condition
+	MachinePoolScalingDownV1Beta2Condition = clusterv1beta1.ScalingDownV1Beta2Condition
 
 	// MachinePoolRemediatingV1Beta2Condition surfaces details about ongoing remediation of the controlled machines, if any.
-	MachinePoolRemediatingV1Beta2Condition = clusterv1.RemediatingV1Beta2Condition
+	MachinePoolRemediatingV1Beta2Condition = clusterv1beta1.RemediatingV1Beta2Condition
 
 	// MachinePoolDeletingV1Beta2Condition surfaces details about ongoing deletion of the controlled machines.
-	MachinePoolDeletingV1Beta2Condition = clusterv1.DeletingV1Beta2Condition
+	MachinePoolDeletingV1Beta2Condition = clusterv1beta1.DeletingV1Beta2Condition
 ).
 */

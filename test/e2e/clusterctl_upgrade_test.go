3929f0df286e7347bb9ccd66dc55b6f5c2d6143e
cprivitere <23177737+cprivitere@users.noreply.github.com>
2025-05-12 14:11:51 -0500

    e2e: bump 1.10 clusterctl upgrade test to 1.33
    Signed-off-by: cprivitere <23177737+cprivitere@users.noreply.github.com>

--- test/e2e/clusterctl_upgrade_test.go
+++ test/e2e/clusterctl_upgrade_test.go
@@ -300,8 +300,8 @@ var _ = Describe("When testing clusterctl upgrades using ClusterClass (v1.10=>cu
 			},
 			// Note: Both InitWithKubernetesVersion and WorkloadKubernetesVersion should be the highest mgmt cluster version supported by the source Cluster API version.
 			// When picking this version, please check also the list of versions known by the source Cluster API version (rif. test/infrastructure/kind/mapper.go).
-			InitWithKubernetesVersion:   "v1.32.2", // NOTE: use v1.33 as soon as it is GA
-			WorkloadKubernetesVersion:   "v1.32.2", // NOTE: use v1.33 as soon as it is GA
+			InitWithKubernetesVersion:   "v1.33.0",
+			WorkloadKubernetesVersion:   "v1.33.0",
 			MgmtFlavor:                  "topology",
 			WorkloadFlavor:              "topology",
 			UseKindForManagementCluster: true,

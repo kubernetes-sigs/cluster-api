f6485cad8e6a1365d2f4f7ae0af8e40cbfc5b512
Christian Schlotter <christian.schlotter@broadcom.com>
2025-05-13 11:26:53 +0200

    Introduce ExtensionConfig v1beta2

---
+++ exp/runtime/api/v1alpha1/conversion.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+
+	runtimev1 "sigs.k8s.io/cluster-api/exp/runtime/api/v1beta2"
+)
+
+func (src *ExtensionConfig) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*runtimev1.ExtensionConfig)
+
+	return Convert_v1alpha1_ExtensionConfig_To_v1beta2_ExtensionConfig(src, dst, nil)
+}
+
+func (dst *ExtensionConfig) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*runtimev1.ExtensionConfig)
+
+	return Convert_v1beta2_ExtensionConfig_To_v1alpha1_ExtensionConfig(src, dst, nil)
+}
--- exp/runtime/api/v1beta2/extensionconfig_types.go
+++ exp/runtime/api/v1beta2/extensionconfig_types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 The Kubernetes Authors.
+Copyright 2025 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1alpha1
+package v1beta2
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -203,6 +203,7 @@ const (
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:path=extensionconfigs,shortName=ext,scope=Cluster,categories=cluster-api
 // +kubebuilder:subresource:status
+// +kubebuilder:storageversion
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="Time duration since creation of ExtensionConfig"
 
 // ExtensionConfig is the Schema for the ExtensionConfig API.

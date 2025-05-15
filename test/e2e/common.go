39ec951dcb713d91e0d22270b34ed2978e0b3ffb
sivchari <shibuuuu5@gmail.com>
2025-05-15 15:12:01 +0900

    add function to check v1beta2 conditions
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- test/e2e/common.go
+++ test/e2e/common.go
@@ -19,11 +19,17 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/blang/semver/v4"
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta2"
 	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
 )
 
@@ -81,4 +87,49 @@ func GetStableReleaseOfMinor(ctx context.Context, minorRelease string) (string, 
 func GetLatestReleaseOfMinor(ctx context.Context, minorRelease string) (string, error) {
 	releaseMarker := fmt.Sprintf(latestReleaseMarkerPrefix, minorRelease)
 	return clusterctl.ResolveRelease(ctx, releaseMarker)
+}
+
+// verifyV1Beta2Conditions checks the Cluster and Machines of a Cluster that
+// the given v1beta2 condition types are set to true without a message, if they exist.
+func verifyV1Beta2Conditions(ctx context.Context, c client.Client, clusterName, clusterNamespace string, v1beta2conditionTypes map[string]struct{}) {
+	cluster := &clusterv1.Cluster{}
+	key := client.ObjectKey{
+		Namespace: clusterNamespace,
+		Name:      clusterName,
+	}
+	Eventually(func() error {
+		return c.Get(ctx, key, cluster)
+	}, 3*time.Minute, 10*time.Second).Should(Succeed(), "Failed to get Cluster object %s", klog.KRef(clusterNamespace, clusterName))
+
+	if len(cluster.Status.Conditions) > 0 {
+		for _, condition := range cluster.Status.Conditions {
+			if _, ok := v1beta2conditionTypes[condition.Type]; !ok {
+				continue
+			}
+			Expect(condition.Status).To(Equal(metav1.ConditionTrue), "The v1beta2 condition %q on the Cluster should be set to true", condition.Type)
+			Expect(condition.Message).To(BeEmpty(), "The v1beta2 condition %q on the Cluster should have an empty message", condition.Type)
+		}
+	}
+
+	machineList := &clusterv1.MachineList{}
+	Eventually(func() error {
+		return c.List(ctx, machineList, client.InNamespace(clusterNamespace),
+			client.MatchingLabels{
+				clusterv1.ClusterNameLabel: clusterName,
+			})
+	}, 3*time.Minute, 10*time.Second).Should(Succeed(), "Failed to list Machines for Cluster %s", klog.KObj(cluster))
+	if len(cluster.Status.Conditions) > 0 {
+		for _, machine := range machineList.Items {
+			if len(machine.Status.Conditions) == 0 {
+				continue
+			}
+			for _, condition := range machine.Status.Conditions {
+				if _, ok := v1beta2conditionTypes[condition.Type]; !ok {
+					continue
+				}
+				Expect(condition.Status).To(Equal(metav1.ConditionTrue), "The v1beta2 condition %q on the Machine %q should be set to true", condition.Type, machine.Name)
+				Expect(condition.Message).To(BeEmpty(), "The v1beta2 condition %q on the Machine %q should have an empty message", condition.Type, machine.Name)
+			}
+		}
+	}
 }

58ff8841211dabe42619d68427daa84646567209
sivchari <shibuuuu5@gmail.com>
2025-05-15 15:21:00 +0900

    apply v1beta2 validation to e2e tests
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- test/e2e/md_remediations.go
+++ test/e2e/md_remediations.go
@@ -27,6 +27,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/utils/ptr"
 
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta2"
 	"sigs.k8s.io/cluster-api/test/framework"
 	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
 	"sigs.k8s.io/cluster-api/util"
@@ -119,6 +120,12 @@ func MachineDeploymentRemediationSpec(ctx context.Context, inputGetter func() Ma
 			Cluster:                   clusterResources.Cluster,
 			WaitForMachineRemediation: input.E2EConfig.GetIntervals(specName, "wait-machine-remediation"),
 		})
+
+		Byf("Verify v1beta2 Available and Ready conditions (if exist) to be true for Cluster and Machines")
+		verifyV1Beta2Conditions(ctx, input.BootstrapClusterProxy.GetClient(), clusterResources.Cluster.Name, clusterResources.Cluster.Namespace,
+			map[string]struct{}{
+				clusterv1.AvailableCondition: {}, clusterv1.ReadyCondition: {},
+			})
 
 		By("PASSED!")
 	})

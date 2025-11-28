d9eb434e419a336c03e2bb6644a0ea0b12a11880
fabriziopandini <fpandini@vmware.com>
2025-11-28 14:40:24 +0100

    Decrease verbosity for MHC log entry

--- internal/controllers/machinehealthcheck/machinehealthcheck_targets.go
+++ internal/controllers/machinehealthcheck/machinehealthcheck_targets.go
@@ -400,7 +400,7 @@ func (r *Reconciler) healthCheckTargets(targets []healthCheckTarget, logger logr
 		}
 
 		if nextCheck > 0 {
-			logger.V(3).Info("Target is likely to go unhealthy", "timeUntilUnhealthy", nextCheck.Truncate(time.Second).String())
+			logger.V(2).Info("Target is likely to go unhealthy", "timeUntilUnhealthy", nextCheck.Truncate(time.Second).String())
 			nextCheckTimes = append(nextCheckTimes, nextCheck)
 			continue
 		}

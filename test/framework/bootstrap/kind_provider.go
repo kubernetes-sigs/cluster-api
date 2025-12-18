cbff1e8e9ab30ad884a42447f6d51b57a00e18a0
Stefan Bueringer <buringerst@vmware.com>
2025-12-18 06:53:05 +0100

    Bump kind to v0.31.0
    Signed-off-by: Stefan Büringer buringerst@vmware.com

--- test/framework/bootstrap/kind_provider.go
+++ test/framework/bootstrap/kind_provider.go
@@ -36,7 +36,7 @@ const (
 	DefaultNodeImageRepository = "kindest/node"
 
 	// DefaultNodeImageVersion is the default Kubernetes version to be used for creating a kind cluster.
-	DefaultNodeImageVersion = "v1.34.0@sha256:7416a61b42b1662ca6ca89f02028ac133a309a2a30ba309614e8ec94d976dc5a"
+	DefaultNodeImageVersion = "v1.35.0@sha256:452d707d4862f52530247495d180205e029056831160e22870e37e3f6c1ac31f"
 )
 
 // KindClusterOption is a NewKindClusterProvider option.

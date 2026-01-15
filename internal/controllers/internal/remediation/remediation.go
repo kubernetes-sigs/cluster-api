6c33508a22dc6e44f726f140c43cc41c742458a3
Andreas Sommer <andreas.sommer87@googlemail.com>
2026-01-15 15:07:57 +0100

    Fix lint (add package comment)

--- internal/controllers/internal/remediation/remediation.go
+++ internal/controllers/internal/remediation/remediation.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package collections contains helpers for remediation related to machine health checks.
 package remediation
 
 import (
